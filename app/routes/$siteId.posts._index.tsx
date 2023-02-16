b7c5d568667bc03f87827acf6b37fac9b9e1f918
Nick Winters <nick@pogseal.com>
2023-02-15 18:48:35 -1000

    post page ui updates

--- app/routes/$siteId.posts._index.tsx
+++ app/routes/$siteId.posts._index.tsx
@@ -4,6 +4,10 @@ import { Form, Link, useLoaderData } from "@remix-run/react";
 import { zx } from "zodix";
 import { z } from "zod";
 import { Plus } from "lucide-react";
+import { formatDistanceStrict } from "date-fns";
+import type { Post } from "payload/generated-types";
+import { Image } from "~/components/Image";
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
 
 export async function loader({
    context: { payload, user },
@@ -20,58 +24,176 @@ export async function loader({
             equals: siteId,
          },
       },
+      sort: "-updatedAt",
    });
 
-   return { posts };
+   const publishedPosts = await payload.find({
+      collection: "posts",
+      user,
+      depth: 2,
+      where: {
+         and: [
+            {
+               isPublished: {
+                  equals: true,
+               },
+               site: {
+                  equals: siteId,
+               },
+            },
+         ],
+      },
+      sort: "-updatedAt",
+   });
+
+   return { posts, publishedPosts };
 }
 
-export default function PostsIndex() {
-   const { posts } = useLoaderData<typeof loader>();
+export const FeedItem = ({ post }: { post: Post }) => {
    return (
-      <div className="mx-auto max-w-[728px] px-3 pt-4 tablet:px-0 laptop:px-3 laptop:pt-8 desktop:px-0">
-         <h1 className="border-color mb-5 border-b-2 pb-2.5 text-3xl font-bold">
+      <>
+         <Link
+            className="relative block py-4"
+            prefetch="intent"
+            to={post.id}
+            key={post.id}
+         >
+            <div className="flex w-full items-center justify-between gap-3 pb-4 text-sm text-gray-500 dark:text-gray-400">
+               <div className="flex items-center gap-3">
+                  <div className="h-6 w-6 overflow-hidden rounded-full">
+                     <Image
+                        width={20}
+                        height={20}
+                        alt={post.title}
+                        options="fit=crop,height=50,width=50,gravity=auto"
+                        className="w-full object-cover laptop:rounded"
+                        //@ts-ignore
+                        url={post?.author.avatar?.url}
+                     />
+                  </div>
+                  {/* @ts-ignore */}
+                  <div className="font-bold">{post.author.username}</div>
+               </div>
+               <div className="text-xs font-bold uppercase">
+                  {post.publishedAt &&
+                     formatDistanceStrict(
+                        new Date(post.updatedAt ?? ""),
+                        new Date(),
+                        {
+                           addSuffix: true,
+                        }
+                     )}
+               </div>
+            </div>
+            <div className="flex items-start gap-5">
+               <div className="relative flex-grow">
+                  <div className="pb-2 text-xl font-bold">{post.title}</div>
+               </div>
+               {post.banner && (
+                  <div className="aspect-[1.4/1] w-32 overflow-hidden rounded laptop:w-40">
+                     <Image
+                        alt={post.title}
+                        options="fit=crop,height=200,gravity=auto"
+                        className="w-full rounded object-cover"
+                        //@ts-ignore
+                        url={post?.banner?.url}
+                     />
+                  </div>
+               )}
+            </div>
+         </Link>
+      </>
+   );
+};
+
+export default function PostsIndex() {
+   const { posts, publishedPosts } = useLoaderData<typeof loader>();
+   return (
+      <div className="mx-auto max-w-[728px] px-3 pt-8 tablet:px-0 laptop:px-3 laptop:pt-10 desktop:px-0">
+         <h1 className="border-color mb-7 flex items-center gap-3 border-b-2 pb-2.5 text-3xl font-bold">
+            <PencilSquareIcon className="h-7 w-7 text-emerald-500" />
             Posts
          </h1>
-         <section className="mb-7 grid grid-cols-2 gap-4 laptop:grid-cols-5">
-            <Form method="post" className="overflow-hidden">
-               <button
-                  className="border-color bg-1 mb-1.5 flex h-40 w-full
-                   items-center justify-center rounded-md border hover:border-blue-200 dark:hover:border-blue-900"
-                  name="intent"
-                  value="createPost"
-                  type="submit"
-               >
-                  <Plus className="h-7 w-7 text-blue-500" />
-               </button>
-               <div className="text-1 pl-1 text-sm font-bold">Blank post</div>
-            </Form>
+         <h2 className="text-1 pb-3 text-sm font-bold uppercase">
+            Create a new post
+         </h2>
+         <section className="pb-8">
+            <div
+               className="border-color flex items-center justify-between rounded-xl border 
+            bg-neutral-100 px-4 py-3 dark:bg-neutral-800"
+            >
+               <div className="flex items-center gap-4">
+                  <div className="text-sm font-bold">Blank post</div>
+                  <span className="h-1.5 w-1.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+                  <span className="text-t text-xs">Start from scratch...</span>
+               </div>
+               <Form method="post" className="flex items-center">
+                  <button
+                     name="intent"
+                     className="flex h-10 w-10 items-center justify-center rounded-full border
+                      border-emerald-200 bg-emerald-50 dark:border-emerald-900/50 dark:bg-emerald-900/20"
+                     value="createPost"
+                     type="submit"
+                  >
+                     <Plus className="h-6 w-6 text-emerald-500" />
+                  </button>
+               </Form>
+            </div>
          </section>
-         <h2 className="pb-2 pl-1 text-lg font-bold">My Posts</h2>
-         <div className="border-color bg-1 divide-y overflow-hidden rounded-md border dark:divide-zinc-700">
+         <h2 className="text-1 pb-3 text-sm font-bold uppercase">My Posts</h2>
+         <section className="border-color divide-y overflow-hidden border-y dark:divide-zinc-700">
             {posts?.docs.length === 0 ? (
-               <div className="p-3 text-sm ">You have no posts...</div>
+               <div className="py-3 text-sm ">No posts...</div>
             ) : (
                posts.docs.map((posts) => (
                   <Link
                      prefetch="intent"
                      to={posts.id}
                      key={posts.id}
-                     className="flex items-center justify-between gap-2 p-3 hover:underline"
+                     className="group flex items-center justify-between gap-2 py-3"
                   >
-                     <span className="truncate">{posts.title}</span>
-                     {posts.isPublished ? (
-                        <span className="rounded bg-green-500 py-1 px-2 text-xs text-white">
-                           Published
-                        </span>
+                     <span className="truncate group-hover:underline">
+                        {posts.title}
+                     </span>
+                     {posts.updatedAt ? (
+                        <div className="flex items-center gap-3">
+                           <time
+                              className="text-1 flex items-center gap-1.5 text-sm"
+                              dateTime={posts?.updatedAt}
+                           >
+                              {formatDistanceStrict(
+                                 new Date(posts?.updatedAt as string),
+                                 new Date(),
+                                 {
+                                    addSuffix: true,
+                                 }
+                              )}
+                           </time>
+                           {posts.isPublished ? (
+                              <span className="h-2 w-2 rounded-full bg-green-500" />
+                           ) : (
+                              <span className="h-2 w-2 rounded-full bg-zinc-500" />
+                           )}
+                        </div>
                      ) : (
-                        <span className="rounded bg-zinc-500 py-1 px-2 text-xs text-white">
-                           Draft
-                        </span>
+                        <span></span>
                      )}
                   </Link>
                ))
             )}
-         </div>
+         </section>
+         <h2 className="text-1 pb-3 pt-12 text-sm font-bold uppercase">
+            Latest
+         </h2>
+         <section className="border-color divide-y overflow-hidden border-y dark:divide-zinc-700">
+            {publishedPosts?.docs.length === 0 ? (
+               <div className="py-3 text-sm ">No posts...</div>
+            ) : (
+               publishedPosts.docs.map((post) => (
+                  <FeedItem key={post.id} post={post} />
+               ))
+            )}
+         </section>
       </div>
    );
 }
