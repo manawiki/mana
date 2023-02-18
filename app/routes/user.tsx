c3edd4bfbdd27a174ddda3341ccbedff68e9e9af
Nick Winters <nick@pogseal.com>
2023-02-17 17:26:41 -1000

    ui details

--- app/routes/user.tsx
+++ app/routes/user.tsx
@@ -32,10 +32,10 @@ export default function UserIndex() {
         desktop:auto-cols-[86px_210px_1fr_334px]"
       >
          <section
-            className="border-color max-laptop:py-4 max-laptop:bg-gradient-to-b max-laptop:w-full  
-                    max-laptop:dark:from-zinc-900 max-laptop:from-zinc-100 max-laptop:dark:to-zinc-800
-                    max-laptop:to-zinc-50 max-laptop:fixed max-laptop:h-20 max-laptop:top-0 laptop:bg-1
-                    relative laptop:border-r"
+            className="border-color laptop:bg-1 relative max-laptop:fixed  
+                    max-laptop:top-0 max-laptop:h-20 max-laptop:w-full
+                    max-laptop:bg-gradient-to-b max-laptop:from-zinc-100 max-laptop:to-zinc-50 max-laptop:py-4 max-laptop:dark:from-zinc-900
+                    max-laptop:dark:to-zinc-800 laptop:border-r"
          >
             <div className="laptop:fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[86px] laptop:overflow-y-auto">
                <SiteSwitcher />
@@ -43,16 +43,16 @@ export default function UserIndex() {
          </section>
          <section>
             <div
-               className="max-laptop:border-t max-laptop:flex max-laptop:h-12 max-laptop:z-20 border-color
-                        bg-1 fixed bottom-0 mx-auto w-full laptop:top-0 laptop:h-full laptop:w-[86px] 
+               className="border-color bg-1 fixed bottom-0 mx-auto
+                        w-full max-laptop:z-20 max-laptop:flex max-laptop:h-12 max-laptop:border-t laptop:top-0 laptop:h-full laptop:w-[86px] 
                         laptop:space-y-4 laptop:overflow-y-auto laptop:border-r laptop:py-6
                         desktop:w-[210px] desktop:space-y-1"
             >
                <LoggedIn>
-                  <Form action="/logout" method="post">
+                  <Form className="px-4" action="/logout" method="post">
                      <button
                         type="submit"
-                        className="flex h-10 w-full items-center justify-between gap-3 px-5 text-left"
+                        className="bg-2 flex h-12 w-full items-center justify-between gap-3 rounded-lg px-5 text-left"
                      >
                         <span className="font-bold">Logout</span>
                         <LogOut className="h-4 w-4 text-red-400 dark:text-red-300" />
