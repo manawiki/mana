c5bc2083d2a15074e7c95232cd636e1db32ac832
homumado <nobisan@gmail.com>
2023-02-17 17:39:30 -1000

    been working on the wrong thing all along, lol

--- app/modules/note/components/NoteLive.tsx
+++ app/modules/note/components/NoteLive.tsx
@@ -1,10 +1,12 @@
-import { evaluate, EvaluateOptions } from "@mdx-js/mdx";
+import type { EvaluateOptions } from "@mdx-js/mdx";
+import { evaluate } from "@mdx-js/mdx";
 import { evaluateSync } from "@mdx-js/mdx";
 import { useMDXComponents } from "@mdx-js/react";
-import { MDXContent, MDXModule } from "mdx/types";
-import { Fragment, useEffect, useMemo, useState } from "react";
+import type { MDXModule } from "mdx/types";
+import { Suspense, useDeferredValue, useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import * as runtime from "react/jsx-dev-runtime";
+import { useDebouncedValue } from "~/hooks";
 import { ErrorFallback } from "./ErrorFallback";
 
 const mdxOptions = {
@@ -15,28 +17,8 @@ const mdxOptions = {
    ...runtime,
 } as EvaluateOptions;
 
-//todo perf comparison with async
-export function NoteLive2({
-   mdx,
-   className = "mdx-content",
-}: {
-   mdx: string;
-   className?: string;
-}) {
-   const { default: Content } = useMemo(() => {
-      console.log("evaluating");
-      return evaluateSync(mdx, mdxOptions);
-   }, [mdx]);
-
-   return (
-      <div className={className}>
-         <ErrorBoundary FallbackComponent={ErrorFallback}>
-            {Content && Content({})}
-         </ErrorBoundary>
-      </div>
-   );
-}
-
+// Client-side render MDX input using async evaluation
+// todo perf comparison with sync
 export function NoteLive({
    mdx,
    className = "mdx-content",
@@ -44,16 +26,53 @@ export function NoteLive({
    mdx: string;
    className?: string;
 }) {
-   // const { default: Content } = useMemo(
-   //    () => evaluateSync(mdx, mdxOptions),
-   //    [mdx]
-   // );
+   const [module, setModule] = useState<MDXModule | null>(null);
 
+   //We'll use deferred values to prevent the MDX from rendering until the user has stopped typing
+   const deferredModule = useDeferredValue(module);
+   const debouncedMDX = useDebouncedValue(mdx, 100);
+
+   useEffect(() => {
+      (async () => {
+         try {
+            const mdxModule = await evaluate(debouncedMDX, mdxOptions);
+            console.log(mdxModule);
+            if (mdxModule) setModule(mdxModule);
+         } catch (e) {
+            console.error(e);
+         }
+      })();
+   }, [debouncedMDX]);
+
+   return (
+      <div className={className}>
+         <Suspense fallback={<h2>Loading...</h2>}>
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+               {deferredModule && <deferredModule.default />}
+            </ErrorBoundary>
+         </Suspense>
+      </div>
+   );
+}
+
+//Clientside render MDX input using sync evaluation
+export function NoteLiveSync({
+   mdx,
+   className = "mdx-content",
+}: {
+   mdx: string;
+   className?: string;
+}) {
    const [module, setModule] = useState<MDXModule | null>(null);
 
    useEffect(() => {
-      const mdxModule = evaluateSyncMDX(mdx);
-      if (mdxModule) setModule(mdxModule);
+      try {
+         const mdxModule = evaluateSync(mdx, mdxOptions);
+         console.log(mdxModule);
+         if (mdxModule) setModule(mdxModule);
+      } catch (e) {
+         console.error(e);
+      }
    }, [mdx]);
 
    return (
@@ -63,14 +82,4 @@ export function NoteLive({
          </ErrorBoundary>
       </div>
    );
-}
-
-function evaluateSyncMDX(mdx: string) {
-   try {
-      const mdxModule = evaluateSync(mdx, mdxOptions);
-      console.log(mdxModule);
-      if (mdxModule) return mdxModule;
-   } catch (e) {
-      console.error(e);
-   }
 }
--- app/modules/note/components/NoteView.tsx
+++ app/modules/note/components/NoteView.tsx
@@ -1,85 +1,43 @@
-import type { EvaluateOptions } from "@mdx-js/mdx";
-import { evaluate } from "@mdx-js/mdx";
-import { evaluateSync } from "@mdx-js/mdx";
-import { useMDXComponents } from "@mdx-js/react";
-import type { MDXModule } from "mdx/types";
-import { Suspense, useDeferredValue, useEffect, useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import * as runtime from "react/jsx-dev-runtime";
-import { useDebouncedValue } from "~/hooks";
-import { ErrorFallback } from "./ErrorFallback";
+import { useMDXComponents } from "@mdx-js/react";
+import { runSync } from "@mdx-js/mdx";
+import { useMemo } from "react";
+import type { Note } from "payload/generated-types";
 
-const mdxOptions = {
-   //    outputFormat: "function-body",
-   useDynamicImport: true,
-   development: true,
-   useMDXComponents,
-   ...runtime,
-} as EvaluateOptions;
-
-// Client-side render MDX input using async evaluation
-// todo perf comparison with sync
-export function NoteLive({
-   mdx,
-   className = "mdx-content",
-}: {
-   mdx: string;
+type NoteViewerProps = {
+   note: Note;
    className?: string;
-}) {
-   const [module, setModule] = useState<MDXModule | null>(null);
+};
 
-   //We'll use deferred values to prevent the MDX from rendering until the user has stopped typing
-   const deferredModule = useDeferredValue(module);
-   const debouncedMDX = useDebouncedValue(mdx, 100);
+export function NoteViewer({
+   note,
+   className = "mdx-content",
+}: NoteViewerProps) {
+   if (!note) return null;
+   if (note.source) return <NoteView note={note} />;
+   if (note.html) return <NoteStatic note={note} />;
 
-   useEffect(() => {
-      (async () => {
-         try {
-            const mdxModule = await evaluate(debouncedMDX, mdxOptions);
-            console.log(mdxModule);
-            if (mdxModule) setModule(mdxModule);
-         } catch (e) {
-            console.error(e);
-         }
-      })();
-   }, [debouncedMDX]);
-
-   return (
-      <div className={className}>
-         <Suspense fallback={<h2>Loading...</h2>}>
-            <ErrorBoundary FallbackComponent={ErrorFallback}>
-               {deferredModule && <deferredModule.default />}
-            </ErrorBoundary>
-         </Suspense>
-      </div>
-   );
+   return null;
 }
 
-//Clientside render MDX input using sync evaluation
-export function NoteLiveSync({
-   mdx,
-   className = "mdx-content",
-}: {
-   mdx: string;
-   className?: string;
-}) {
-   const [module, setModule] = useState<MDXModule | null>(null);
-
-   useEffect(() => {
-      try {
-         const mdxModule = evaluateSync(mdx, mdxOptions);
-         console.log(mdxModule);
-         if (mdxModule) setModule(mdxModule);
-      } catch (e) {
-         console.error(e);
-      }
-   }, [mdx]);
-
-   return (
-      <div className={className}>
-         <ErrorBoundary FallbackComponent={ErrorFallback}>
-            {module && <module.default />}
-         </ErrorBoundary>
-      </div>
+//todo perf comparison with async
+export function NoteView({ note, className = "mdx-content" }: NoteViewerProps) {
+   const { default: Content } = useMemo(
+      () => runSync(note.source as string, { ...runtime, useMDXComponents }),
+      [note.source]
    );
+
+   return <div className={className}>{Content()}</div>;
+}
+
+export function NoteStatic({
+   note,
+   className = "mdx-content",
+}: NoteViewerProps) {
+   return note?.html ? (
+      <div
+         className={className}
+         dangerouslySetInnerHTML={{ __html: note.html }}
+      />
+   ) : null;
 }
