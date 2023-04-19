49946810e70927982fb613a1258cad95577660b7
Nick Winters <nick@pogseal.com>
2023-04-19 16:31:12 -0700

    bucket as env

--- db/collections/CustomUsers.ts
+++ db/collections/CustomUsers.ts
@@ -1,11 +1,43 @@
+import {
+   authenticatedAndStaff,
+   isStaffFieldLevel,
+   isStaffOrSelf,
+} from "../access";
 import type { CollectionConfig } from "payload/types";
 export const Users: CollectionConfig = {
    slug: "users",
    auth: true,
+   access: {
+      read: () => true,
+      create: () => true,
+      delete: authenticatedAndStaff,
+      update: isStaffOrSelf,
+   },
    fields: [
       {
          name: "id",
          type: "text",
+      },
+      {
+         name: "roles",
+         saveToJWT: true,
+         type: "select",
+         hasMany: true,
+         defaultValue: ["user"],
+         access: {
+            create: isStaffFieldLevel,
+            update: isStaffFieldLevel,
+         },
+         options: [
+            {
+               label: "Staff",
+               value: "staff",
+            },
+            {
+               label: "User",
+               value: "user",
+            },
+         ],
       },
    ],
 };
