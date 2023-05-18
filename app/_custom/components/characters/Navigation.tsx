e7df0f6ee6f12cb89ab0aa345461b335beed609a
Nick Winters <nick@pogseal.com>
2023-05-18 02:59:14 -0700

    showcase polish

--- app/_custom/components/characters/Navigation.tsx
+++ app/_custom/components/characters/Navigation.tsx
@@ -1,13 +1,16 @@
 export const Navigation = ({ links }: any) => {
    return (
       <>
-         <div className="flex justify-between gap-x-1 mb-2 items-center">
+         <div className="mb-3 grid grid-cols-2 items-center justify-between gap-2 laptop:grid-cols-6">
             {links?.map((l: any) => (
                <a
+                  key={l.name}
                   href={`#${l.link}`}
-                  className="border-color flex items-center self-stretch shadow-1 rounded-md border shadow-sm p-2 py-3 w-full text-center justify-center font-bold hover:bg-gray-500 hover:bg-opacity-10"
+                  className="border-color bg-2 text-1 overflow-hidden rounded-full
+                  border bg-yellow-50 p-2 text-center text-sm
+                  font-bold transition dark:bg-yellow-900/20"
                >
-                  <div className="inline-flex leading-none">{l.name}</div>
+                  {l.name}
                </a>
             ))}
          </div>
