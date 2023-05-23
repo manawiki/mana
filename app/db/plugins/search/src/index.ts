85f0cf0af3d3a662a452f9b6476c552b3895fd3e
Nick Winters <nick@pogseal.com>
2023-05-23 02:36:33 -0700

    seed search core

--- app/db/plugins/search/src/index.ts
+++ app/db/plugins/search/src/index.ts
@@ -29,6 +29,7 @@ export const generateSearchCollection = (
             {
                name: "name",
                type: "text",
+               index: true,
                admin: {
                   readOnly: true,
                },
