9f3ebad3cd35f5dba3047b636fe58ae5480b66f7
Dimbreath <dimbreath@gmail.com>
2023-05-25 05:07:38 -0300

    Enable graphql playground in prod temporarily

--- app/db/payload.custom.config.ts
+++ app/db/payload.custom.config.ts
@@ -33,6 +33,9 @@ const adapter = s3Adapter({
 });
 
 export default buildConfig({
+   graphQL: {
+      disablePlaygroundInProduction: false
+    },
    serverURL:
       process.env.NODE_ENV == "development"
          ? "http://localhost:4000"
