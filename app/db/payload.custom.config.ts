f6ea7529f38f9f837f8cf649cd3fa9faea6fbb35
Nick Winters <nick@pogseal.com>
2023-05-25 15:18:56 -0700

    update version

--- app/db/payload.custom.config.ts
+++ app/db/payload.custom.config.ts
@@ -52,9 +52,6 @@ export default buildConfig({
          titleSuffix: "Mana",
       },
    },
-   graphQL: {
-      disable: true,
-   },
    plugins: [
       cloudStorage({
          collections: {
