28461a3100822d1c9e4f0eb7374c2a2a7b55375d
fred <nobisan@gmail.com>
2023-05-25 19:17:07 -0700

    no longer purge here

--- core.server.ts
+++ core.server.ts
@@ -91,8 +91,6 @@ async function startCore() {
       "*",
       process.env.NODE_ENV === "development"
          ? (req, res, next) => {
-            //   purgeRequireCache();
-
               return createRequestHandler({
                  build: require(BUILD_DIR),
                  mode: process.env.NODE_ENV,
