6462470bd4c9ca655b02db3f6bdb88e18bffa9f7
fred <nobisan@gmail.com>
2023-06-12 12:52:12 -0700

    Split out payload custom types for better type inference

--- scripts/clean-payload-types.js
+++ scripts/clean-payload-types.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 
 // We should probably avoid hard-coding the path like this
-const file = "./app/db/payload-types.ts";
+process?.env?.PAYLOAD_CONFIG_PATH === "./app/db/payload.custom.config.ts"
+   ? cleanFile("./app/db/payload-custom-types.ts")
+   : cleanFile("./app/db/payload-types.ts");
 
-// Read the file
-let content = fs.readFileSync(file, "utf-8");
+function cleanFile(file) {
+   console.log(`Cleaning ${file}`);
 
-// Make your changes to the content
-content = content.replace(/string \| /g, "");
+   // Read the file
+   let content = fs.readFileSync(file, "utf-8");
 
-// Write the file
-fs.writeFileSync(file, content, "utf-8");
+   // Make your changes to the content
+   content = content.replace(/string \| /g, "").replace(/string\[\] \| /g, "");
+
+   // Write the file
+   fs.writeFileSync(file, content, "utf-8");
+}
