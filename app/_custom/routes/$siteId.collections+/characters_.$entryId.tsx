15edd4f144306ea225cd7ea66fd2282e0af60081
Nick Winters <nick@pogseal.com>
2023-06-14 23:29:12 -0700

    Update characters_.$entryId.tsx

--- app/_custom/routes/$siteId.collections+/characters_.$entryId.tsx
+++ app/_custom/routes/$siteId.collections+/characters_.$entryId.tsx
@@ -125,6 +125,15 @@ export default function CharacterEntry() {
                entryId={entryId}
                defaultValue={embeddedContent}
             />
+            <EntryContentEmbed
+               title="Recommended Light Cones"
+               sectionId="light-cones"
+               collectionEntity="characters"
+               siteId={siteId}
+               fetcher={fetcher}
+               entryId={entryId}
+               defaultValue={embeddedContent}
+            />
             <div id="traces"></div>
             {/* Traces / Skills */}
             <H2 text="Traces" />
