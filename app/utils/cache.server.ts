fc9b153f09fda264c9b23c9887c4967ff8d78408
xHomu <84349818+xHomu@users.noreply.github.com>
2023-06-24 06:22:04 -0700

    Cache local payload api demo (#184)
    * Implement a lru-cache for local api

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -1,8 +1,7 @@
 import { LRUCache } from "lru-cache";
 import { remember } from "./remember.server";
-// import NProgress from "nprogress";
-// import nProgressStyles from "~/styles/nprogress.css";
 
+// Setup an in memory lru-cache for api calls, this will be cleared on server restart.
 export const lruCache = remember(
    "lruCache",
    new LRUCache({
@@ -11,22 +10,52 @@ export const lruCache = remember(
    })
 );
 
-// Setup a lru-cache for layout data, so we don't have to fetch it every time
+/**
+ * Setup a lru-cache for layout data, so we don't have to fetch it every time. Params are based on browser fetch api.
+ * @param url - rest api endpoint `https://${process.env.PAYLOAD_PUBLIC_SITE_ID}-db.mana.wiki/api/${collectionId}/${entryId}?depth=1`
+ * @param init - browser fetch api init object for headers, body, etc
+ * @returns  the result of the fetch or its cached value
+ */
 export async function fetchWithCache(url: string, init?: RequestInit) {
-     //The key could be graphql, in that case we'll use init.body instead. We should also delete any spacing characters
-     const key = init?.body ? (init.body as string).replace(/\s/g, "").replace(/\n/g, " ")
-     : url;
+   //The key could be graphql, in that case we'll use init.body instead. We should also delete any spacing characters
+   const key = init?.body
+      ? (init.body as string).replace(/\s/g, "").replace(/\n/g, " ")
+      : url;
 
-    const cached = lruCache.get(key);
-    if (cached) {
-        return cached;
-    }
-    return fetch(url, init).then((res) => {
-        const response = res.json();
-        lruCache.set(key, response);
+   const cached = lruCache.get(key);
+   if (cached) {
+      return cached;
+   }
+   return fetch(url, init).then((res) => {
+      const response = res.json();
+      lruCache.set(key, response);
 
-        //log cache size, trim length to terminal width
-        console.log(`API Cached ${lruCache.size}: ${key}`.substring(0, 80));
-        return response;
-    });
+      //log cache size, trim length to terminal width
+      console.log(`API Cached ${lruCache.size}: ${key}`.substring(0, 80));
+      return response;
+   });
+}
+
+/**
+ * Use this to cache a function return. Use this only for public api calls, not for private api calls.
+ * @param func  await cacheThis(func () => payload.find({...}));
+ * @returns  the result of the function or its cached value
+ */
+export async function cacheThis<T>(func: () => Promise<T>) {
+   //the key is the function stringified
+   let key = func.toString().replace(/\s/g, "").replace(/\n/g, " ");
+
+   // if the function is payload api, we'll use the body instead
+   key = key.split("(")?.slice(2)?.join("(") ?? key;
+
+   const cached = lruCache.get(key);
+   if (cached) {
+      return cached as T;
+   }
+   const result = await func();
+   lruCache.set(key, result);
+
+   //log cache size, trim length to terminal width
+   console.log(`API Cached ${lruCache.size}: ${key}`.substring(0, 80));
+   return result;
 }
