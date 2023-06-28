ece7aa535290dcb1acc1d197c84d91c66fbe3f0f
xHomu <84349818+xHomu@users.noreply.github.com>
2023-06-28 15:40:54 -0700

    refactor & make EntryContentEmbed components lazily loaded
    Might want to move the new EntryViewer to somewhere and use it as a generic lazy slate component.

---
+++ app/modules/collections/components/EntryViewer.tsx
@@ -0,0 +1,23 @@
+import type { RenderElementProps } from "slate-react";
+import { Editable, Slate, withReact } from "slate-react";
+import Block from "~/modules/editor/blocks/Block";
+import Leaf from "~/modules/editor/blocks/Leaf";
+import { useMemo, useCallback } from "react";
+import { createEditor } from "slate";
+
+//Isolate the Slate Viewer to make it easier to tree-shake with lazily loaded components
+export const EntryViewer = ({ content }: { content: any }) => {
+   const editor = useMemo(() => withReact(createEditor()), []);
+   const renderElement = useCallback((props: RenderElementProps) => {
+      return <Block {...props} />;
+   }, []);
+   return (
+      <Slate editor={editor} value={content}>
+         <Editable
+            renderElement={renderElement}
+            renderLeaf={Leaf}
+            readOnly={true}
+         />
+      </Slate>
+   );
+};
