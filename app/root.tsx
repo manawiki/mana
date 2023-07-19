f717c77dc1272a6979dd41f0186e1d9a47019159
xHomu <84349818+xHomu@users.noreply.github.com>
2023-07-18 21:57:18 -0700

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -263,7 +263,9 @@ function App() {
             <ScrollRestoration />
             {isBot ? null : <Scripts />}
             <LiveReload />
-            {process.env.NODE_ENV === "development" && <RemixDevTools />}
+            {process.env.NODE_ENV === "development" && !isMobileApp && (
+               <RemixDevTools />
+            )}
          </body>
       </html>
    );
