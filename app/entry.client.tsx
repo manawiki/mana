58a90e9aabd459ae24cec88d3469a4d95ac1945a
xHomu <84349818+xHomu@users.noreply.github.com>
2023-08-04 06:44:04 -0700

    eslint

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,21 +1,23 @@
-import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+
+import { RemixBrowser } from "@remix-run/react";
 import i18next from "i18next";
+import { hydrateRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
+
 import { initI18nextClient } from "./utils/i18n/i18next.client";
 
 async function hydrate() {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <I18nextProvider i18n={i18next}>
-        <StrictMode>
-          <RemixBrowser />
-        </StrictMode>
-      </I18nextProvider>
-    );
-  });
+   startTransition(() => {
+      hydrateRoot(
+         document,
+         <I18nextProvider i18n={i18next}>
+            <StrictMode>
+               <RemixBrowser />
+            </StrictMode>
+         </I18nextProvider>
+      );
+   });
 }
 
 initI18nextClient(hydrate);
