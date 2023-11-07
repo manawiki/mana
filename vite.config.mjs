6e0eda14d19a9ba2d9574b29c8791511e4cb2fb7
xHomu <84349818+xHomu@users.noreply.github.com>
2023-11-07 12:45:59 -0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,8 +10,8 @@ export default defineConfig({
    plugins: [
       remix({
          ignoredRouteFiles: ["**/.*"],
-         tailwind: true,
-         postcss: true,
+         // tailwind: true,
+         // postcss: true,
          // appDirectory: "app",
          // assetsBuildDirectory: "public/build",
          // publicPath: "/build/",
