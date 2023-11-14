c2cab9b90bbd7510ddc933310403a4bcbb6d26ac
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2023-11-13 18:51:06 -0800

    [Endfield] Material Collection Initialization

---
+++ app/_custom/collections/_item-obtain-ways.ts
@@ -0,0 +1,41 @@
+import type { CollectionConfig } from "payload/types";
+
+import { isStaff } from "../../access/user";
+
+export const _ItemObtainWays: CollectionConfig = {
+   slug: "_item-obtain-ways",
+   labels: { singular: "_item-obtain-way", plural: "_item-obtain-ways" },
+   admin: {
+      group: "Custom",
+      useAsTitle: "name",
+   },
+   access: {
+      create: isStaff, //udpate in future to allow site admins as well
+      read: () => true,
+      update: isStaff, //udpate in future to allow site admins as well
+      delete: isStaff, //udpate in future to allow site admins as well
+   },
+   fields: [
+      {
+         name: "id",
+         type: "text",
+      },
+      {
+         name: "data_key",
+         type: "text",
+      },
+      {
+         name: "name",
+         type: "text",
+      },
+      {
+         name: "icon",
+         type: "upload",
+         relationTo: "images",
+      },
+      {
+         name: "checksum",
+         type: "text",
+      },
+   ],
+};
