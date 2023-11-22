be0fc41a78e57886536445d7d3b3c316adbe83ce
Corey Chambers <coreychambers123@gmail.com>
2023-11-22 13:41:33 -0500

    Seed homeContents data upon site creation

--- app/db/collections/Sites.ts
+++ app/db/collections/Sites.ts
@@ -1,6 +1,50 @@
-import type { CollectionConfig } from "payload/types";
+import type { CollectionAfterChangeHook, CollectionConfig } from "payload/types";
 
 import { isStaff, isStaffFieldLevel, isLoggedIn } from "../../access/user";
+const afterCreateSite: CollectionAfterChangeHook = async ({
+   doc,
+   req: { payload },
+   operation, // name of the operation i.e. 'create', 'update'
+}) => {
+   try {
+      // On site creation, create a default homeContents entry
+      if (operation === "create") {
+         const siteId = doc.slug;
+         await payload.create({
+            collection: "homeContents",
+            data: {
+               site: siteId,
+               _status: "published",
+               content: [
+                  {
+                     id: "viwnxpInwb-HSLnwixmaU",
+                     type: "h2",
+                     children: [
+                        {
+                           text: "Welcome to Mana",
+                        }
+                     ]
+                  },
+                  {
+                     id: "jsowPnsUbN-UmsYfOpFtY",
+                     type: "paragraph",
+                     children: [
+                        {
+                           text: "This page was automatically generated during site creation, and it looks like it hasn't been replaced yet."
+                        }
+                     ]
+                  }
+               ]
+            },
+         });
+      }
+   } catch (err: unknown) {
+      console.log("ERROR");
+      payload.logger.error(`${err}`);
+   }
+
+   return doc;
+};
 
 export const sitesSlug = "sites";
 export const Sites: CollectionConfig = {
@@ -173,4 +217,7 @@ export const Sites: CollectionConfig = {
          type: "text",
       },
    ],
+   hooks: {
+      afterChange: [afterCreateSite],
+   },
 };
