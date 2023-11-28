ce8d19b9cb79ed235aa7be385775fc7a6af74b5b
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2023-11-28 13:50:19 -0800

    [ZZZ] Renamed Characters to Agents

--- app/_custom/import/importscripts.js
+++ app/_custom/import/importscripts.js
@@ -1,4 +1,4 @@
 #"import_collection_data": "cross-env PAYLOAD_CONFIG_PATH=./app/db/payload.custom.config.ts ts-node -T app/_custom/import/import_collection_data.ts"
 
-# Characters
-pnpm import_collection_data collection:characters,filename:Character.json,idname:data_key,sync:false,overwrite:false
+# Agents
+pnpm import_collection_data collection:agents,filename:Agent.json,idname:data_key,sync:false,overwrite:false
