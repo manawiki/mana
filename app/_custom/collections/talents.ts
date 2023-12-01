3c7d083109fcf2cf2e879eb38d4090850d7f454e
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2023-11-30 17:49:32 -0800

    [ZZZ] Talent Collection Initialized
    New:
    - _Rarities
    - Talents
    
    Updated:
    - Agents (rarity and talents field added)
    
    All data imported!

---
+++ app/_custom/collections/talents.ts
@@ -0,0 +1,48 @@
+import type { CollectionConfig } from "payload/types";
+
+import { isStaff } from "../../access/user";
+
+export const Talents: CollectionConfig = {
+  slug: "talents",
+  labels: { singular: "talent", plural: "talents" },
+  admin: {
+    group: "Custom",
+    useAsTitle: "name",
+  },
+  access: {
+    create: isStaff, //udpate in future to allow site admins as well
+    read: () => true,
+    update: isStaff, //udpate in future to allow site admins as well
+    delete: isStaff, //udpate in future to allow site admins as well
+  },
+  fields: [
+    {
+      name: "id",
+      type: "text",
+    },
+    {
+      name: "data_key",
+      type: "text",
+    },
+    {
+      name: "name",
+      type: "text",
+    },
+    {
+      name: "sort",
+      type: "number",
+    },
+    {
+      name: "desc",
+      type: "textarea",
+    },
+    {
+      name: "desc_flavor",
+      type: "textarea",
+    },
+    {
+      name: "checksum",
+      type: "text",
+    },
+  ],
+};
--- app/_custom/import/importscripts.js
+++ app/_custom/import/importscripts.js
@@ -1,6 +1,7 @@
 #"import_collection_data": "cross-env PAYLOAD_CONFIG_PATH=./app/db/payload.custom.config.ts ts-node -T app/_custom/import/import_collection_data.ts"
 
 # _Collections for Agent
+pnpm import_collection_data collection:_rarities,filename:_Rarity.json,idname:data_key,sync:false,overwrite:false
 pnpm import_collection_data collection:_character-camps,filename:_CharacterCamp.json,idname:data_key,sync:false,overwrite:false
 pnpm import_collection_data collection:_damage-elements,filename:_DamageElement.json,idname:data_key,sync:false,overwrite:false
 pnpm import_collection_data collection:_damage-types,filename:_DamageType.json,idname:data_key,sync:false,overwrite:false
@@ -12,3 +13,6 @@ pnpm import_collection_data collection:agents,filename:Agent.json,idname:data_ke
 
 # Skills
 pnpm import_collection_data collection:skills,filename:Skill.json,idname:data_key,sync:false,overwrite:false
+
+# Talents
+pnpm import_collection_data collection:talents,filename:Talent.json,idname:data_key,sync:false,overwrite:false
