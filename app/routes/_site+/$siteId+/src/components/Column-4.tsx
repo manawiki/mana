3472de723e579b6b8d2524c9c190844f0b3e1573
Nick Winters <nick@pogseal.com>
2023-12-16 12:41:06 -0800

    trending ui updates

--- app/routes/_site+/$siteId+/src/components/Column-4.tsx
+++ app/routes/_site+/$siteId+/src/components/Column-4.tsx
@@ -4,7 +4,6 @@ import { Image } from "~/components";
 import { Icon } from "~/components/Icon";
 import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/Tooltip";
 import type { Site } from "~/db/payload-types";
-import { LoggedOut } from "~/routes/_auth+/src/components";
 import { AdUnit } from "~/routes/_site+/$siteId+/src/components/Ramp";
 
 import { PinnedList, PrimaryMenuLinks } from "./Menu";
@@ -23,68 +22,64 @@ export function ColumnFour({ site }: { site: Site }) {
                   <PinnedList site={site} />
                </section>
                <div className="relative">
-                  <div className="z-20 relative">
-                     <div className="m-4">
-                        <div className="grid grid-cols-3 gap-4">
-                           <div className="dark:bg-dark350/70 bg-white dark:shadow-zinc-800 shadow-sm border border-color px-3 py-1.5 rounded-lg">
-                              <div className="text-xs font-bold text-center">
-                                 {site.followers}
-                              </div>
-                              <div className="text-xs text-1 text-center">
-                                 Followers
-                              </div>
+                  <div className="sticky top-0 w-full left-0 bg-zinc-50 dark:bg-dark350 laptop:dark:bg-bg2Dark">
+                     <section className="grid grid-cols-3 gap-4 p-4 relative z-20">
+                        <div className="dark:bg-dark350/70 bg-white dark:shadow-zinc-800 shadow-sm border border-color px-3 py-1.5 rounded-lg">
+                           <div className="text-xs font-bold text-center">
+                              {site.followers}
                            </div>
-                           <div className="dark:bg-dark350/70 bg-white dark:shadow-zinc-800 shadow-sm border border-color px-3 py-1.5 rounded-lg">
-                              <div className="text-xs font-bold text-center">
-                                 0
-                              </div>
-                              <div className="text-xs text-1 text-center">
-                                 Posts
-                              </div>
-                           </div>
-                           <div className="dark:bg-dark350/70 bg-white dark:shadow-zinc-800 shadow-sm border border-color px-3 py-1.5 rounded-lg">
-                              <div className="text-xs text-center font-bold">
-                                 3,000
-                              </div>
-                              <div className="text-xs text-center text-1">
-                                 Entries
-                              </div>
+                           <div className="text-xs text-1 text-center">
+                              Followers
                            </div>
                         </div>
-                        {/* <div className="text-xs text-1 pb-2">Lvl 1</div>
-                        <div className="rounded-full h-3 w-full dark:bg-dark500 bg-teal-50 relative overflow-hidden">
-                           <span className="absolute top-0 left-0 bg-teal-500 w-40 h-full" />
-                        </div> */}
-                     </div>
+                        <div className="dark:bg-dark350/70 bg-white dark:shadow-zinc-800 shadow-sm border border-color px-3 py-1.5 rounded-lg">
+                           <div className="text-xs font-bold text-center">
+                              -
+                           </div>
+                           <div className="text-xs text-1 text-center">
+                              Posts
+                           </div>
+                        </div>
+                        <div className="dark:bg-dark350/70 bg-white dark:shadow-zinc-800 shadow-sm border border-color px-3 py-1.5 rounded-lg">
+                           <div className="text-xs text-center font-bold">
+                              -
+                           </div>
+                           <div className="text-xs text-center text-1">
+                              Entries
+                           </div>
+                        </div>
+                     </section>
+                     {/* <div className="text-xs text-1 pb-2">Lvl 1</div>
+                                 <div className="rounded-full h-3 w-full dark:bg-dark500 bg-teal-50 relative overflow-hidden">
+                                    <span className="absolute top-0 left-0 bg-teal-500 w-40 h-full" />
+                                 </div> */}
                      {site?.banner && (
-                        <div
-                           className="bg-3-sub border border-zinc-300 dark:border-zinc-700 flex items-center rounded-lg mx-4
-                        justify-center bg-white h-[170px] relative overflow-hidden shadow-sm shadow-1"
+                        <section
+                           className="bg-3-sub border border-zinc-200 dark:border-zinc-700 flex items-center rounded-lg mx-4
+                              justify-center bg-white h-[170px] overflow-hidden shadow-sm shadow-1 relative z-20"
                         >
                            <Image
+                              //@ts-ignore
                               url={site?.banner?.url}
                               options="aspect_ratio=16:9&height=400"
                               alt="Site Banner"
                               className=""
                            />
-                           <span className="bg-gradient-to-t from-zinc-900 to-transparent  w-full h-full absolute top-0 left-0 z-10" />
+                           <span className="bg-gradient-to-t dark:from-zinc-900 from-white to-transparent  w-full h-full absolute top-0 left-0 z-10" />
                            <div
-                              className="absolute [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]
-                           text-white bottom-0 font-bold text-sm left-0 p-4 z-20"
+                              className="absolute dark:[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] [text-shadow:_0_1px_0_rgb(255_255_255_/_40%)]
+                           dark:text-white bottom-0 text-sm left-0 p-4 z-20 space-y-1"
                            >
-                              {site.name}
+                              <span className="font-bold">{site.name}</span>
+                              {site.about && (
+                                 <div className="dark:text-zinc-400 text-xs">
+                                    {site.about}
+                                 </div>
+                              )}
                            </div>
-                        </div>
+                        </section>
                      )}
-                     <section
-                        className="dark:border-zinc-700 border-color 
-                     border dark:bg-dark350/70 rounded-xl m-4 py-4 px-4"
-                     >
-                        {site.about && (
-                           <div className="text-1 text-xs">{site.about}</div>
-                        )}
-                     </section>
-                     <section className="ml-4 py-4 border-b border-color dark:border-zinc-700/40">
+                     <section className="ml-4 py-4 relative z-20">
                         <div className="flex items-center pb-3">
                            <span className="text-1 text-sm font-bold">
                               Contributors
@@ -97,7 +92,7 @@ export function ColumnFour({ site }: { site: Site }) {
                                     <Tooltip key={user.id}>
                                        <TooltipTrigger>
                                           <div
-                                             className="bg-3 shadow-1 flex h-9 w-9 items-center justify-center
+                                             className="bg-3 shadow-1 flex h-9 w-9 items-center justify-center shadow-1
                                           overflow-hidden rounded-full border border-zinc-200 shadow-sm dark:border-zinc-600"
                                           >
                                              {user.avatar?.url ? (
@@ -165,8 +160,18 @@ export function ColumnFour({ site }: { site: Site }) {
                            </Tooltip>
                         </div>
                      </section>
-                     <section className="ml-5 pt-6 pb-4">
-                        <div className="flex items-center pb-3 gap-1.5">
+                     {/* <section className="flex items-center justify-center pt-2 relative z-20">
+                        <div className="w-[300px] h-[250px] rounded-xl bg-white dark:bg-dark400 laptop:dark:bg-zinc-800"></div>
+                     </section> */}
+                     <AdUnit
+                        adType="desktopSquareATF"
+                        selectorId="rightSidebarBottomUnit"
+                        enableAds={site.enableAds}
+                        className="flex [&>div]:my-4 items-center justify-center"
+                     />
+                     {/* @ts-ignore */}
+                     {site?.trendingPages && site?.trendingPages.length > 0 && (
+                        <div className="flex items-center gap-1.5 ml-4 py-4 relative z-20">
                            <Icon
                               name="flame"
                               className="dark:text-zinc-400 text-zinc-500"
@@ -176,88 +181,57 @@ export function ColumnFour({ site }: { site: Site }) {
                               Trending
                            </span>
                         </div>
+                     )}
+                     <span
+                        className="bg-gradient-to-t dark:from-dark350 laptop:dark:from-bg2Dark dark:to-transparent 
+                           from-zinc-50 to-transparent w-full h-full absolute top-0 left-0 z-10"
+                     />
+                     <div
+                        className="pattern-dots absolute left-0 top-0 z-0 h-full
+                           w-full pattern-bg-white pattern-zinc-400 pattern-opacity-10 
+                           pattern-size-2 dark:pattern-zinc-500 dark:pattern-bg-bg2Dark"
+                     />
+                  </div>
+                  {/* @ts-ignore */}
+                  {site?.trendingPages && site?.trendingPages.length > 0 && (
+                     <section className="ml-5">
                         <div className="divide-y divide-color dark:divide-zinc-700/40 border-y border-color dark:border-zinc-700/40 font-semibold">
-                           <div className="flex items-center gap-2 px-3 py-2.5 pl-0 group">
-                              <div className="text-sm flex-grow group-hover:underline">
-                                 Charizard
-                              </div>
-                              <div className="w-7 h-7 flex-none flex items-center justify-center">
-                                 <Icon
-                                    name="database"
-                                    className="dark:text-zinc-500 text-zinc-400"
-                                    size={12}
-                                 />
-                              </div>
-                           </div>
-                           <div className="flex items-center gap-2 px-3 py-2.5 pl-0 group">
-                              <div className="text-sm flex-grow group-hover:underline">
-                                 Longer title that can keep going on like this
-                              </div>
-                              <div className="w-7 h-7 flex-none flex items-center justify-center">
-                                 <Icon
-                                    name="component"
-                                    className="dark:text-zinc-500 text-zinc-400"
-                                    size={14}
-                                 />
-                              </div>
-                           </div>
-                           <div className="flex items-center gap-2 px-3 py-2.5 pl-0 group">
-                              <div className="text-sm flex-grow group-hover:underline">
-                                 Venusaur
-                              </div>
-                              <div className="w-7 h-7 flex-none flex items-center justify-center">
-                                 <Icon
-                                    name="database"
-                                    className="dark:text-zinc-500 text-zinc-400"
-                                    size={12}
-                                 />
-                              </div>
-                           </div>
-                           <div className="flex items-center gap-2 px-3 py-2.5 pl-0 group">
-                              <div className="text-sm flex-grow group-hover:underline">
-                                 Raichu
-                              </div>
-                              <div className="w-7 h-7 flex-none flex items-center justify-center">
-                                 <Icon
-                                    name="database"
-                                    className="dark:text-zinc-500 text-zinc-400"
-                                    size={12}
-                                 />
-                              </div>
-                           </div>
-                           <div className="flex items-center gap-2 px-3 py-2.5 pl-0 group">
-                              <div className="text-sm flex-grow group-hover:underline">
-                                 Another one but with a longer title that can
-                                 keep going on like this
-                              </div>
-                              <div className="w-7 h-7 flex-none flex items-center justify-center">
-                                 <Icon
-                                    name="pen-square"
-                                    className="dark:text-zinc-500 text-zinc-400"
-                                    size={12}
-                                 />
-                              </div>
-                           </div>
+                           {/* @ts-ignore */}
+                           {site?.trendingPages?.map((row: any) => (
+                              <Link
+                                 to={row.path}
+                                 key={row.path}
+                                 className="flex items-center gap-2 px-3 py-2 pl-0 group"
+                              >
+                                 <div className="text-sm flex-grow group-hover:underline">
+                                    {row.data.name}
+                                 </div>
+                                 <div
+                                    className="h-[26px] w-[26px] flex items-center justify-center rounded-full bg-white
+                                    border border-color-sub shadow-sm shadow-1 overflow-hidden dark:bg-dark400"
+                                 >
+                                    {row.data?.icon?.url ? (
+                                       <Image
+                                          width={40}
+                                          height={40}
+                                          url={row.data?.icon?.url}
+                                          options="aspect_ratio=1:1&height=54&width=54"
+                                          alt={row.data.name}
+                                       />
+                                    ) : (
+                                       <Icon
+                                          name="component"
+                                          className="dark:text-zinc-500 text-zinc-400 mx-auto"
+                                          size={16}
+                                       />
+                                    )}
+                                 </div>
+                              </Link>
+                           ))}
                         </div>
                      </section>
-                  </div>
-                  <span
-                     className="bg-gradient-to-t dark:from-bg2Dark dark:to-transparent from-zinc-50 to-transparent
-                              w-full h-full absolute top-0 left-0 z-10"
-                  />
-                  <div
-                     className="pattern-dots absolute left-0 top-0 z-0 h-full
-                     w-full pattern-bg-white pattern-zinc-400 pattern-opacity-10 
-                     pattern-size-2 dark:pattern-zinc-500 dark:pattern-bg-bg2Dark"
-                  />
+                  )}
                </div>
-               <div className="flex-grow" />
-               <AdUnit
-                  adType="desktopSquareATF"
-                  selectorId="rightSidebarBottomUnit"
-                  enableAds={site.enableAds}
-                  className="flex [&>div]:my-4 items-center justify-center"
-               />
             </div>
          </div>
       </section>
