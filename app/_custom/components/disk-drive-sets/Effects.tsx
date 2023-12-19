ff1da02f342af5bbec7e091a96267e0658761930
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2023-12-18 20:08:02 -0800

    [ZZZ] Disk Drive Set Templating Initialization
    New templates:
    - Disk-drive-sets: List page
    - Disk-drive-sets: Template page
    -- Main
    -- Effects
    
    Display for multiple possible rarities per disk drive set + filtering.

---
+++ app/_custom/components/disk-drive-sets/Effects.tsx
@@ -0,0 +1,73 @@
+import type { DiskDriveSet as DiskDriveSetType } from "payload/generated-custom-types";
+import { H2, Image } from "~/components";
+import { useState } from "react";
+
+export function Effects({ data: char }: { data: DiskDriveSetType }) {
+  const desc_icons = [
+    {
+      id: "Icon_Normal",
+      icon: "https://static.mana.wiki/zzz/IconRoleSkillKeyNormal.png",
+    },
+    {
+      id: "Icon_Special",
+      icon: "https://static.mana.wiki/zzz/IconRoleSkillKeySpecial.png",
+    },
+    {
+      id: "Icon_Evade",
+      icon: "https://static.mana.wiki/zzz/IconRoleSkillKeyEvade.png",
+    },
+    {
+      id: "Icon_Ultimate",
+      icon: "https://static.mana.wiki/zzz/IconRoleSkillKeyUltimate.png",
+    },
+    {
+      id: "Icon_Switch",
+      icon: "https://static.mana.wiki/zzz/IconRoleSkillKeySwitch.png",
+    },
+    {
+      id: "Icon_JoyStick",
+      icon: "https://static.mana.wiki/zzz/IconRoleSkillKeyJoyStick.png",
+    },
+  ];
+
+  const talents = char.talent;
+
+  const [talentLevel, setTalentLevel] = useState(0);
+
+  // Some general CSS stuff
+  const set_effects = char.set_effect;
+
+  return (
+    <>
+      <H2 text="Set Effects" />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<style>
+                                  div.zzz-skill-description > span {
+                                     background-color: rgb(50,50,50);
+                                     padding-top: 2px;
+                                     padding-right: 2px;
+                                     padding-left: 2px;
+                                     padding-bottom: 1px;
+                                     margin-right: 2px;
+                                     margin-left: 2px;
+                                     border-radius: 2px;
+                                  }
+                               </style>`,
+        }}
+      ></div>
+
+      {/* Description */}
+      {set_effects?.map((effect: any) => {
+        return (
+          <>
+            <div className="grid grid-cols-8 bg-zinc-50 dark:bg-dark350 shadow-sm shadow-1 border border-color-sub rounded-lg px-3 py-2 my-2">
+              <div className="col-span-1">{effect.num}-Pc:</div>
+              <div className="col-span-7">{effect.desc}</div>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+}
