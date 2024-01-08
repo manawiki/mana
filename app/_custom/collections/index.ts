4d2247da7941d8e7b54289e82ac4423a79d8ad87
Nick Winters <nick@pogseal.com>
2024-01-08 15:41:25 -0800

    v2_template fix

--- app/_custom/collections/index.ts
+++ app/_custom/collections/index.ts
@@ -1,5 +1,37 @@
-export const CustomCollections = [];
+import { Factions } from './factions';
+import { Materials } from './materials';
+import { MaterialTypes } from './materialTypes';
+import { Medals } from './medals';
+import { MedalSets } from './medalSets';
+import { MedalTypes } from './medalTypes';
+import { Operators } from './operators';
+import { OperatorTags } from './operatorTags';
+import { Positions } from './positions';
+import { Professions } from './professions';
+import { Rarities } from './rarities';
 
-export const CustomSearchCollections = [];
+export const CustomCollections = [
+    Factions,
+    Materials,
+    MaterialTypes,
+    Medals,
+    MedalSets,
+    MedalTypes,
+    Operators,
+    OperatorTags,
+    Positions,
+    Professions,
+    Rarities
+];
 
-export const CustomDefaultPriorities = {};
+export const CustomSearchCollections = [
+    "materials",
+    "medals",
+    "operators"
+];
+
+export const CustomDefaultPriorities = {
+    materials: 50,
+    medals: 40,
+    operators: 60
+};
