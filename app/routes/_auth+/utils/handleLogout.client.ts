2298cd5cdcf316647c2c1aa4fa182e50eb682a42
xHomu <84349818+xHomu@users.noreply.github.com>
2024-01-09 14:15:49 -0800

    fix logout, we might not need sitePath anymore

--- app/routes/_auth+/utils/handleLogout.client.ts
+++ app/routes/_auth+/utils/handleLogout.client.ts
@@ -1,10 +1,15 @@
-export async function handleLogout(sitePath: string) {
-   const safeSitePath = sitePath.replace("http", "https");
+import { log } from "console";
 
-   const logoutUrl =
+export async function handleLogout(sitePath: string) {
+   let logoutUrl =
       process.env.NODE_ENV === "development"
          ? `http://localhost:3000/api/users/logout`
-         : `${safeSitePath}api/users/logout`;
+         : `https://mana.wiki/api/users/logout`;
+
+   if (document?.location?.origin)
+      logoutUrl = `${document.location.origin}/api/users/logout`;
+
+   console.log(logoutUrl);
 
    try {
       await fetch(logoutUrl, {
