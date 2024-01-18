8a9ee97fe964e915475688016ecf9f555cf04545
Nick Winters <nick@pogseal.com>
2024-01-18 10:04:43 -1000

    modal updates

--- app/components/Alert.tsx
+++ app/components/Alert.tsx
@@ -14,15 +14,15 @@ import clsx from "clsx";
 import { Text } from "./Text";
 
 const sizes = {
-   xs: "sm:max-w-xs",
-   sm: "sm:max-w-sm",
-   md: "sm:max-w-md",
-   lg: "sm:max-w-lg",
-   xl: "sm:max-w-xl",
-   "2xl": "sm:max-w-2xl",
-   "3xl": "sm:max-w-3xl",
-   "4xl": "sm:max-w-4xl",
-   "5xl": "sm:max-w-5xl",
+   xs: "tablet:max-w-xs",
+   sm: "tablet:max-w-sm",
+   md: "tablet:max-w-md",
+   lg: "tablet:max-w-lg",
+   xl: "tablet:max-w-xl",
+   "2xl": "tablet:max-w-2xl",
+   "3xl": "tablet:max-w-3xl",
+   "4xl": "tablet:max-w-4xl",
+   "5xl": "tablet:max-w-5xl",
 };
 
 export function Alert({
@@ -48,11 +48,13 @@ export function Alert({
                leaveFrom="opacity-100"
                leaveTo="opacity-0"
             >
-               <div className="fixed inset-0 flex w-screen justify-center overflow-y-auto bg-zinc-950/15 px-2 py-2 focus:outline-0 sm:px-6 sm:py-8 lg:px-8 lg:py-16 dark:bg-zinc-950/50" />
+               <div
+                  className="z-50 fixed inset-0 flex w-screen justify-center overflow-y-auto bg-zinc-950/25 px-2 py-2 
+               focus:outline-0 tablet:px-6 tablet:py-8 desktop:px-8 desktop:py-16 dark:bg-zinc-950/50"
+               />
             </HeadlessTransitionChild>
-
             <HeadlessTransitionChild
-               className="fixed inset-0 w-screen overflow-y-auto pt-6 sm:pt-0"
+               className="fixed z-50 inset-0 w-screen overflow-y-auto pt-6 tablet:pt-0"
                enter="ease-out duration-100"
                enterFrom="opacity-0"
                enterTo="opacity-100"
@@ -60,13 +62,13 @@ export function Alert({
                leaveFrom="opacity-100"
                leaveTo="opacity-0"
             >
-               <div className="grid min-h-full grid-rows-[1fr_auto_1fr] justify-items-center p-8 sm:grid-rows-[1fr_auto_3fr] sm:p-4">
+               <div className="grid min-h-full grid-rows-[1fr_auto_1fr] justify-items-center p-8 tablet:grid-rows-[1fr_auto_3fr] tablet:p-4">
                   <HeadlessTransitionChild
                      as={HeadlessDialogPanel}
                      className={clsx(
                         className,
                         sizes[size],
-                        "row-start-2 w-full rounded-2xl bg-white p-8 shadow-lg ring-1 ring-zinc-950/10 sm:rounded-2xl sm:p-6 dark:bg-zinc-900 dark:ring-white/10 forced-colors:outline",
+                        "row-start-2 w-full rounded-2xl bg-3 p-8 shadow-lg ring-1 ring-zinc-950/10 tablet:rounded-2xl tablet:p-6 dark:ring-white/20 forced-colors:outline",
                      )}
                      enter="ease-out duration-100"
                      enterFrom="scale-95"
@@ -93,7 +95,7 @@ export function AlertTitle({
          {...props}
          className={clsx(
             className,
-            "text-balance text-center text-base/6 font-semibold text-zinc-950 sm:text-wrap sm:text-left sm:text-sm/6 dark:text-white",
+            "text-balance text-center text-base/6 font-semibold tablet:text-wrap tablet:text-left tablet:text-sm/6",
          )}
       />
    );
@@ -109,7 +111,7 @@ export function AlertDescription({
          {...props}
          className={clsx(
             className,
-            "mt-2 text-pretty text-center sm:text-left",
+            "mt-2 text-pretty text-center tablet:text-left",
          )}
       />
    );
@@ -131,7 +133,7 @@ export function AlertActions({
          {...props}
          className={clsx(
             className,
-            "mt-6 flex flex-col-reverse items-center justify-end gap-3 *:w-full sm:mt-4 sm:flex-row sm:*:w-auto",
+            "mt-6 flex flex-col-reverse items-center justify-end gap-3 *:w-full tablet:mt-4 tablet:flex-row tablet:*:w-auto",
          )}
       />
    );
