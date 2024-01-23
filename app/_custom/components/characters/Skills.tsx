7a5b46285c34b4668b6008ad18a145971a9ba60f
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2024-01-22 19:22:28 -0800

    [Endfield] Character Entry: Factory Skills + Skill Cost
    New Sections:
    - Factory Skills
    - Skill Cost (skill upgrade costs)
    
    Fixes:
    - Break Cost: Added rarity coloring for items, also used in Skill Cost component
    - Skills: Fixed alignment display for icons on mobile.

--- app/_custom/components/characters/Skills.tsx
+++ app/_custom/components/characters/Skills.tsx
@@ -4,13 +4,13 @@ import { Image } from "~/components/Image";
 import { useState } from "react";
 
 export function Skills({ data: char }: { data: CharacterType }) {
-   const skills = char?.skills;
-   return (
-      <>
-         <H2 text="Skills" />
-         <div
-            dangerouslySetInnerHTML={{
-               __html: `<style>
+  const skills = char?.skills;
+  return (
+    <>
+      <H2 text="Skills" />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<style>
                                   div.endfield-skill-description > span {
                                      background-color: rgb(50,50,50);
                                      padding-top: 2px;
@@ -22,74 +22,71 @@ export function Skills({ data: char }: { data: CharacterType }) {
                                      border-radius: 2px;
                                   }
                                </style>`,
-            }}
-         ></div>
-         {skills?.map((sk: any) => {
-            const [skillLevel, setSkillLevel] = useState(0);
+        }}
+      ></div>
+      {skills?.map((sk: any) => {
+        const [skillLevel, setSkillLevel] = useState(0);
 
-            const s = sk.skill;
-            console.log(skillLevel);
-            return (
-               <>
-                  <div className="bg-zinc-50 dark:bg-dark350 shadow-sm shadow-1 border border-color-sub rounded-lg px-3 py-1 grid grid-cols-12 gap-2 my-3">
-                     <div className="inline-block mr-2 rounded-full bg-zinc-300 dark:bg-zinc-700 h-fit">
-                        <Image
-                           height={50}
-                           className="object-contain"
-                           url={s.icon?.url}
-                           options="height=50"
-                           alt={"SkillIcon"}
-                        />
-                     </div>
-                     <div className="inline-block mr-2 col-span-11">
-                        {/* Header */}
-                        <div className="">
-                           <div className="font-bold inline-block mr-2">
-                              {s?.name}
-                           </div>
-                           <div className="inline-block mr-2 text-xs">
-                              Lv: {s?.levels[skillLevel].level}
-                           </div>
-                           <div className="inline-block mr-2 text-xs">
-                              Cost: {s?.levels[skillLevel].cost_val}
-                           </div>
-                           <div className="inline-block mr-2 text-xs">
-                              CD: {s?.levels[skillLevel].cool_down}
-                           </div>
-                           <div className="inline-block mr-2 text-xs">
-                              Max Charge Time:{" "}
-                              {s?.levels[skillLevel].max_charge_time}
-                           </div>
-                        </div>
-
-                        {/* Slider */}
-                        {s?.levels?.length - 1 > 0 ? (
-                           <input
-                              aria-label="Level Slider"
-                              className="h-1 w-full
-                               rounded bg-zinc-200 align-middle accent-zinc-500 outline-none dark:bg-zinc-700"
-                              type="range"
-                              min="0"
-                              max={s?.levels?.length - 1}
-                              value={skillLevel}
-                              onChange={(event) =>
-                                 setSkillLevel(parseInt(event.target.value))
-                              }
-                           ></input>
-                        ) : null}
-
-                        {/* Description */}
-                        <div
-                           className="text-sm mt-2 p-1 endfield-skill-description"
-                           dangerouslySetInnerHTML={{
-                              __html: `${s.levels[skillLevel].description}`,
-                           }}
-                        ></div>
-                     </div>
+        const s = sk.skill;
+        console.log(skillLevel);
+        return (
+          <>
+            <div className="bg-zinc-50 dark:bg-dark350 shadow-sm shadow-1 border border-color-sub rounded-lg px-2 py-1 grid grid-cols-6 laptop:grid-cols-12 gap-2 my-3">
+              <div className="flex mr-2 rounded-full bg-zinc-300 dark:bg-zinc-700 h-12 w-12">
+                <Image
+                  height={50}
+                  className="object-contain"
+                  url={s.icon?.url}
+                  options="height=50"
+                  alt={"SkillIcon"}
+                />
+              </div>
+              <div className="inline-block mr-2 col-span-5 laptop:col-span-11">
+                {/* Header */}
+                <div className="">
+                  <div className="font-bold inline-block mr-2">{s?.name}</div>
+                  <div className="inline-block mr-2 text-xs">
+                    Lv: {s?.levels[skillLevel].level}
                   </div>
-               </>
-            );
-         })}
-      </>
-   );
+                  <div className="inline-block mr-2 text-xs">
+                    Cost: {s?.levels[skillLevel].cost_val}
+                  </div>
+                  <div className="inline-block mr-2 text-xs">
+                    CD: {s?.levels[skillLevel].cool_down}
+                  </div>
+                  <div className="inline-block mr-2 text-xs">
+                    Max Charge Time: {s?.levels[skillLevel].max_charge_time}
+                  </div>
+                </div>
+
+                {/* Slider */}
+                {s?.levels?.length - 1 > 0 ? (
+                  <input
+                    aria-label="Level Slider"
+                    className="h-1 w-full
+                               rounded bg-zinc-200 align-middle accent-zinc-500 outline-none dark:bg-zinc-700"
+                    type="range"
+                    min="0"
+                    max={s?.levels?.length - 1}
+                    value={skillLevel}
+                    onChange={(event) =>
+                      setSkillLevel(parseInt(event.target.value))
+                    }
+                  ></input>
+                ) : null}
+
+                {/* Description */}
+                <div
+                  className="text-sm mt-2 p-1 endfield-skill-description"
+                  dangerouslySetInnerHTML={{
+                    __html: `${s.levels[skillLevel].description}`,
+                  }}
+                ></div>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
 }
