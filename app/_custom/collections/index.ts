18a8a35f23fe85f46f96782ada8f1919f9d116f3
Nick Winters <nick@pogseal.com>
2024-01-24 11:26:15 -1000

    Revert "Merge fixes (#235)"
    This reverts commit 129fe20f2f70527589837f8375164ba6933c370e.

--- app/_custom/collections/index.ts
+++ app/_custom/collections/index.ts
@@ -1,17 +1,5 @@
-import { Dolls } from "./dolls";
-import { Skills } from "./skills";
+export const CustomCollections = [];
 
-export const CustomDefaultPriorities = {
-    dolls: 70,
-    skills: 60,
-};
+export const CustomSearchCollections = [];
 
-export const CustomSearchCollections = [
-    "dolls",
-    "skills",
-];
-
-export const CustomCollections = [
-    Dolls,
-    Skills,
-];
+export const CustomDefaultPriorities = {};
