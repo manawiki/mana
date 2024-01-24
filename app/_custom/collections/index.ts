129fe20f2f70527589837f8375164ba6933c370e
pogseal <nick@pogseal.com>
2024-01-24 11:25:28 -1000

    Merge fixes (#235)
    * Update config.json
    
    * Initial empty collections
    
    * Added missing bust image field
    
    * Added skills collection
    
    * Added more fields to dolls collection
    
    * Merge branch 'main' of https://github.com/manasites/gfl2
    
    * Added skills relationship dolls <-> skills
    
    * Revert seed files
    
    * fix merge
    
    * Delete config.json
    
    ---------
    
    Co-authored-by: Dimbreath <dimbreath@gmail.com>
    Co-authored-by: xHomu <84349818+xHomu@users.noreply.github.com>

--- app/_custom/collections/index.ts
+++ app/_custom/collections/index.ts
@@ -1,5 +1,17 @@
-export const CustomCollections = [];
+import { Dolls } from "./dolls";
+import { Skills } from "./skills";
 
-export const CustomSearchCollections = [];
+export const CustomDefaultPriorities = {
+    dolls: 70,
+    skills: 60,
+};
 
-export const CustomDefaultPriorities = {};
+export const CustomSearchCollections = [
+    "dolls",
+    "skills",
+];
+
+export const CustomCollections = [
+    Dolls,
+    Skills,
+];
---
+++ app/_custom/collections/skills.ts
@@ -0,0 +1,40 @@
+import type { CollectionConfig } from "payload/types";
+
+import { isStaff } from "../../access/user";
+
+export const Skills: CollectionConfig = {
+   slug: "skills",
+   labels: { singular: "skill", plural: "skills" },
+   admin: {
+      group: "Custom",
+      useAsTitle: "name",
+   },
+   access: {
+      create: isStaff,
+      read: () => true,
+      update: isStaff,
+      delete: isStaff,
+   },
+   fields: [
+      {
+         name: "id",
+         type: "text",
+      },
+      {
+         name: "name",
+         type: "text",
+      },
+      {
+         name: "icon",
+         type: "upload",
+         relationTo: "images",
+      },
+      {
+         name: "doll",
+         type: "relationship",
+         relationTo: "dolls",
+         hasMany: false,
+         index: true,
+      },
+   ],
+};
