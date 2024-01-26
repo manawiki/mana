2c90c501a70c2658e8c5dec3fe3392ed490d1f33
Nick Winters <nick@pogseal.com>
2024-01-26 10:29:45 -1000

    Update inngest-client.ts

--- app/routes/inngest+/utils/inngest-client.ts
+++ app/routes/inngest+/utils/inngest-client.ts
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "core" });
+export const inngest = new Inngest({
+   id: "core",
+   serveHost: "https://mana.wiki",
+});
