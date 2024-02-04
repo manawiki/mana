f7d54efa5be9bb813964a2816f95f385afcdf688
xHomu <84349818+xHomu@users.noreply.github.com>
2024-02-03 23:50:52 -0800

    fix payload type cleanup script

--- scripts/clean-payload-types.js
+++ scripts/clean-payload-types.js
@@ -12,13 +12,9 @@ function cleanFile(file) {
    let content = fs.readFileSync(file, "utf-8");
 
    // Fix Payload depth issues
-   content = content.replace(/string \| /g, "").replace(/string\[\] \| /g, "");
-
-   // Fix payload 2.0.4 type error
-   //      '_statTypes': _statType ->       '_statTypes': _StatType
-   content = content.replace(/(:\s_)([a-z])/g, (match, p1, p2) => {
-      return p1 + p2.toUpperCase();
-   });
+   content = content
+      .replace(/string \| (?=[A-Z])/g, "")
+      .replace(/string\[\] \| (?=[A-Z])/g, "");
 
    // Write the file
    fs.writeFileSync(file, content, "utf-8");
