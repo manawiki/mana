9631daf1f0770d401ec53d50ffc693179c42a758
xHomu <84349818+xHomu@users.noreply.github.com>
2024-02-04 01:04:37 -0800

    update access

--- app/_custom/collections/_statTypes.ts
+++ app/_custom/collections/_statTypes.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from "payload/types";
 
-import { isStaff } from "../../access/user";
+import { isStaff } from "../../db/collections/users/access";
 
 export const _StatTypes: CollectionConfig = {
    slug: "_statTypes",
--- app/_custom/collections/lightCones.ts
+++ app/_custom/collections/lightCones.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from "payload/types";
 
-import { isStaff } from "../../access/user";
+import { isStaff } from "../../db/collections/users/access";
 
 export const LightCones: CollectionConfig = {
    slug: "lightCones",
