034c023447f897f04938eeab8142a9c1695b5221
Nick Winters <nick@pogseal.com>
2024-02-07 07:45:02 -1000

    fix import paths

--- app/_custom/collections/medalTypes.ts
+++ app/_custom/collections/medalTypes.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from "payload/types";
 
-import { isStaff } from "../../access/user";
+import { isStaff } from "../../db/collections/users/access";
 
 export const MedalTypes: CollectionConfig = {
    slug: "medalTypes",
