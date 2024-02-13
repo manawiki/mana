18b749b0c95beab7de599894e8f31bd8395232d1
xHomu <84349818+xHomu@users.noreply.github.com>
2024-02-12 16:40:47 -0800

    Revert "Add EditorView suspense boundary to limit the damage when hydration mismatch"
    This reverts commit fe253e1e26631c75d9c44b975e313cd3f0c4bb5f.

--- app/routes/_editor+/core/components/EditorView.tsx
+++ app/routes/_editor+/core/components/EditorView.tsx
@@ -1,10 +1,8 @@
-import { Suspense, useCallback, useMemo } from "react";
+import { useCallback, useMemo } from "react";
 
 import { createEditor } from "slate";
 import type { RenderElementProps } from "slate-react";
 import { Slate, Editable, withReact } from "slate-react";
-
-import { Icon } from "~/components/Icon";
 
 // eslint-disable-next-line import/no-cycle
 import { EditorBlocks } from "./EditorBlocks";
@@ -16,27 +14,12 @@ export function EditorView({ data }: { data: any }) {
       return <EditorBlocks {...props} />;
    }, []);
    return (
-      <Suspense
-         //we'll use this suspense boundary to limit the damage when hydration mismatch
-         fallback={<Loading />}
-      >
-         <Slate editor={editor} initialValue={data}>
-            <Editable
-               renderElement={renderElement}
-               renderLeaf={Leaf}
-               readOnly={true}
-            />
-         </Slate>
-      </Suspense>
+      <Slate editor={editor} initialValue={data}>
+         <Editable
+            renderElement={renderElement}
+            renderLeaf={Leaf}
+            readOnly={true}
+         />
+      </Slate>
    );
 }
-
-const Loading = () => (
-   <div className="flex items-center justify-center py-10">
-      <Icon
-         name="loader-2"
-         size={20}
-         className="animate-spin dark:text-zinc-500 text-zinc-400"
-      />
-   </div>
-);
