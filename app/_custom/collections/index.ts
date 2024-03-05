bee498b224c903c854d2ee0e72c5f9693987ee54
Dimbreath <dimbreath@gmail.com>
2024-03-05 03:51:07 -0300

    WIP weapon collections

--- app/_custom/collections/index.ts
+++ app/_custom/collections/index.ts
@@ -1,21 +1,27 @@
 import { Elements } from "./elements";
+import { Rarities } from "./rarities";
 import { ThropyCategories } from "./thropyCategories";
 import { ThropyGroups } from "./thropyGroups";
 import { Thropies } from "./trophies";
+import { Weapons } from "./weapons";
 import { WeaponTypes } from "./weaponTypes";
 
 export const CustomCollections = [
     Elements,
+    Rarities,
     ThropyCategories,
     ThropyGroups,
     Thropies,
+    Weapons,
     WeaponTypes,
 ];
 
 export const CustomSearchCollections = [
     "thropies",
+    "weapons",
 ];
 
 export const CustomDefaultPriorities = {
     thropies: 1,
+    weapons: 2,
 };
