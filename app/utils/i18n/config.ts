830259df2f061001d5fb57fd66d1dfa75f47f401
xHomu <84349818+xHomu@users.noreply.github.com>
2024-03-07 09:43:26 -0800

    Update config.ts

--- app/utils/i18n/config.ts
+++ app/utils/i18n/config.ts
@@ -6,4 +6,6 @@ export const config = {
    fallbackLng: "en",
    // The default namespace of i18next is "translation", but you can customize it here
    defaultNS: "common",
+   // Disabling suspense is recommended
+   react: { useSuspense: false },
 };
