b5f362d3632f8d1e7299b3049bc820b01e681fe4
Nick Winters <nick@pogseal.com>
2024-04-01 09:16:19 -0700

    mobile logout + color fixes

--- app/routes/_user+/components/UserMenuItems.tsx
+++ app/routes/_user+/components/UserMenuItems.tsx
@@ -2,7 +2,7 @@ import { UserMenuLink } from "./UserMenuLink";
 
 export function UserMenuItems() {
    return (
-      <div className="space-y-1.5">
+      <div className="space-y-1.5 flex-grow">
          <UserMenuLink text="Account" icon="user" to="/user/account" />
          <UserMenuLink text="Appearance" icon="palette" to="/user/appearance" />
          <UserMenuLink text="Billing" icon="credit-card" to="/user/billing" />
