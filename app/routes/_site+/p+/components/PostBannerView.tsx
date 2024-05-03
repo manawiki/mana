ff6534c4d9070c7fef9e571331432ad2e9d98377
Nick Winters <nick@pogseal.com>
2024-05-03 10:01:40 -0700

    Table view fixes

--- app/routes/_site+/p+/components/PostBannerView.tsx
+++ app/routes/_site+/p+/components/PostBannerView.tsx
@@ -7,6 +7,7 @@ export function PostBannerView({ post }: { post: Post }) {
          <div className="relative tablet:rounded-lg overflow-hidden max-tablet:-mx-3 max-w-[800px] mx-auto my-6">
             <Image
                alt="Post Banner"
+               className="h-full w-full object-cover"
                options="aspect_ratio=1.9:1"
                url={post?.banner?.url}
             />
