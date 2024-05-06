1237e387777cf498f028e984191032b256c6939e
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2024-05-06 15:15:23 -0700

    [starrail] Hidden Skill Support Added
    Ex Acheron has multiple skills / traces that should be hidden; added handling for hide_ui field to hide skills that should not be shown.

--- app/_custom/routes/_site.c+/characters_.$entryId.tsx
+++ app/_custom/routes/_site.c+/characters_.$entryId.tsx
@@ -23,220 +23,223 @@ export { entryMeta as meta };
 export { ErrorBoundary } from "~/components/ErrorBoundary";
 
 export async function loader({
-   context: { payload, user },
-   params,
-   request,
+  context: { payload, user },
+  params,
+  request,
 }: LoaderFunctionArgs) {
-   const fetchCharacterData = fetchEntry({
-      payload,
-      params,
-      request,
-      user,
-      gql: {
-         query: CharacterQuery,
-      },
-   });
+  const fetchCharacterData = fetchEntry({
+    payload,
+    params,
+    request,
+    user,
+    gql: {
+      query: CharacterQuery,
+    },
+  });
 
-   const fetchSkillTreeData = fetchEntry({
-      payload,
-      params,
-      request,
-      user,
-      gql: {
-         query: SkillTreeQuery,
-      },
-   });
+  const fetchSkillTreeData = fetchEntry({
+    payload,
+    params,
+    request,
+    user,
+    gql: {
+      query: SkillTreeQuery,
+    },
+  });
 
-   const [{ entry }, data] = await Promise.all([
-      fetchCharacterData,
-      fetchSkillTreeData,
-   ]);
+  const [{ entry }, data] = await Promise.all([
+    fetchCharacterData,
+    fetchSkillTreeData,
+  ]);
 
-   return json({
-      entry,
-      skillTreeData: data?.entry?.data?.skillTree?.docs as SkillTreeType[],
-   });
+  return json({
+    entry,
+    skillTreeData: data?.entry?.data?.skillTree?.docs as SkillTreeType[],
+  });
 }
 
 const SECTIONS = {
-   main: CharacterStatBlock,
-   traces: Traces,
-   tree: SkillTree,
-   eidolons: Eidolons,
-   promotion: PromotionCost,
-   materials: TotalMaterialCost,
-   gallery: ImageGallery,
-   profile: Profile,
-   story: Story,
-   "voice-lines": VoiceLines,
+  main: CharacterStatBlock,
+  traces: Traces,
+  tree: SkillTree,
+  eidolons: Eidolons,
+  promotion: PromotionCost,
+  materials: TotalMaterialCost,
+  gallery: ImageGallery,
+  profile: Profile,
+  story: Story,
+  "voice-lines": VoiceLines,
 };
 
 export default function CharacterEntry() {
-   const { entry, skillTreeData } = useLoaderData<typeof loader>();
+  const { entry, skillTreeData } = useLoaderData<typeof loader>();
 
-   const data = {
-      character: entry.data.character as Character,
-      skillTree: skillTreeData,
-   };
+  const data = {
+    character: entry.data.character as Character,
+    skillTree: skillTreeData,
+  };
 
-   return <Entry customComponents={SECTIONS} customData={data} />;
+  console.log(data);
+
+  return <Entry customComponents={SECTIONS} customData={data} />;
 }
 
 const CharacterQuery = gql`
-   query ($entryId: String!) {
-      character: Character(id: $entryId) {
-         id
-         name
-         image_draw {
-            url
-         }
-         element {
-            icon {
-               url
-            }
-         }
-         path {
-            name
-            icon {
-               url
-            }
-            icon_small {
-               url
-            }
-            data_key
-         }
-         rarity {
-            icon {
-               url
-            }
-         }
-         stats {
-            label
-            data
-         }
-         stats_csv
-         traces {
-            name
-            desc_type
-            icon {
-               url
-            }
-            description_per_level {
-               description
-            }
-         }
-         icon {
-            url
-         }
-         image_full {
-            url
-         }
-         image_full_bg {
-            url
-         }
-         image_full_front {
-            url
-         }
-         image_action {
-            url
-         }
-         image_round_icon {
-            url
-         }
-         eidolons {
-            image {
-               url
-            }
-            icon {
-               url
-            }
-            name
-            rank
-            description
-         }
-         promotion_cost {
-            max_level
-            material_qty {
-               materials {
-                  id
-                  icon {
-                     url
-                  }
-                  rarity {
-                     display_number
-                  }
-                  name
-               }
-               qty
-            }
-         }
-         cv_cn
-         cv_jp
-         cv_kr
-         cv_en
-         camp
-         story {
-            title
-            unlock
-            text
-         }
-         voice_lines {
-            title
-            text
-            voice_en {
-               url
-            }
-            voice_jp {
-               url
-            }
-            voice_cn {
-               url
-            }
-            voice_kr {
-               url
-            }
-         }
+  query ($entryId: String!) {
+    character: Character(id: $entryId) {
+      id
+      name
+      image_draw {
+        url
       }
-   }
+      element {
+        icon {
+          url
+        }
+      }
+      path {
+        name
+        icon {
+          url
+        }
+        icon_small {
+          url
+        }
+        data_key
+      }
+      rarity {
+        icon {
+          url
+        }
+      }
+      stats {
+        label
+        data
+      }
+      stats_csv
+      traces {
+        name
+        desc_type
+        icon {
+          url
+        }
+        description_per_level {
+          description
+        }
+        hide_ui
+      }
+      icon {
+        url
+      }
+      image_full {
+        url
+      }
+      image_full_bg {
+        url
+      }
+      image_full_front {
+        url
+      }
+      image_action {
+        url
+      }
+      image_round_icon {
+        url
+      }
+      eidolons {
+        image {
+          url
+        }
+        icon {
+          url
+        }
+        name
+        rank
+        description
+      }
+      promotion_cost {
+        max_level
+        material_qty {
+          materials {
+            id
+            icon {
+              url
+            }
+            rarity {
+              display_number
+            }
+            name
+          }
+          qty
+        }
+      }
+      cv_cn
+      cv_jp
+      cv_kr
+      cv_en
+      camp
+      story {
+        title
+        unlock
+        text
+      }
+      voice_lines {
+        title
+        text
+        voice_en {
+          url
+        }
+        voice_jp {
+          url
+        }
+        voice_cn {
+          url
+        }
+        voice_kr {
+          url
+        }
+      }
+    }
+  }
 `;
 
 const SkillTreeQuery = gql`
-   query ($entryId: JSON) {
-      skillTree: SkillTrees(
-         limit: 1000
-         where: { character: { equals: $entryId } }
-      ) {
-         docs {
-            anchor
-            icon {
-               url
-            }
-            name
-            affected_skill {
-               description_per_level {
-                  description
-               }
-            }
+  query ($entryId: JSON) {
+    skillTree: SkillTrees(
+      limit: 1000
+      where: { character: { equals: $entryId } }
+    ) {
+      docs {
+        anchor
+        icon {
+          url
+        }
+        name
+        affected_skill {
+          description_per_level {
             description
-            level_up_cost {
-               material_qty {
-                  id
-                  materials {
-                     id
-                     icon {
-                        url
-                     }
-                     rarity {
-                        display_number
-                     }
-                     name
-                  }
-                  qty
-               }
+          }
+        }
+        description
+        level_up_cost {
+          material_qty {
+            id
+            materials {
+              id
+              icon {
+                url
+              }
+              rarity {
+                display_number
+              }
+              name
             }
-            req_ascension
-            req_level
-         }
+            qty
+          }
+        }
+        req_ascension
+        req_level
       }
-   }
+    }
+  }
 `;
