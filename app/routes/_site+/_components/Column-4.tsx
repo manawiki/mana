24893f7c75509e8d4738adf09b7a3bbfe9f15410
Nick Winters <nick@pogseal.com>
2024-05-07 10:03:18 -0400

    Update Column-4.tsx

--- app/routes/_site+/_components/Column-4.tsx
+++ app/routes/_site+/_components/Column-4.tsx
@@ -8,6 +8,7 @@ import type { loader as siteLoaderType } from "~/routes/_site+/_layout";
 
 import { Contributors } from "./Contributors";
 import { PinnedList, PrimaryMenuLinks } from "./Menu";
+import { Tooltip, TooltipTrigger, TooltipContent } from "~/components/Tooltip";
 
 export function ColumnFour() {
    const { site } = useLoaderData<typeof siteLoaderType>() || {};
@@ -56,40 +57,48 @@ export function ColumnFour() {
                               </div>
                            </div>
                         </section>
-
-                        <section
+                        {site?.banner && (
+                           <section
+                              className="flex items-center rounded-lg mx-4
+                              justify-center overflow-hidden shadow dark:shadow-zinc-800 relative z-20"
+                           >
+                              <Image
+                                 height={600}
+                                 url={site?.banner?.url}
+                                 options="aspect_ratio=1.9:1"
+                                 alt="Site Banner"
+                                 loading="lazy"
+                              />
+                           </section>
+                        )}
+                        <div
                            className={clsx(
-                              site?.banner
-                                 ? "h-[170px] border-zinc-300 dark:border-zinc-700/80"
-                                 : "h-[70px] border-color",
-                              `bg-3 border flex items-center rounded-lg mx-4
-                              justify-center bg-white overflow-hidden shadow-sm shadow-1 relative z-20`,
+                              site?.banner ? "mt-4" : "mt-0",
+                              "relative z-20 mx-4 pb-4 space-y-1 border-b border-dotted border-zinc-200 dark:border-zinc-700",
                            )}
                         >
-                           {site?.banner && (
-                              <>
-                                 <Image
-                                    //@ts-ignore
-                                    url={site?.banner?.url}
-                                    options="aspect_ratio=1.6:1&height=400"
-                                    alt="Site Banner"
-                                    loading="lazy"
-                                 />
-                                 <span className="bg-gradient-to-t dark:from-zinc-900/90 from-white/70 to-transparent  w-full h-full absolute top-0 left-0 z-10" />
-                              </>
-                           )}
                            <div
-                              className="absolute dark:[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] [text-shadow:_0_1px_0_rgb(255_255_255_/_40%)]
-                           dark:text-white text-zinc-700 bottom-0 text-sm left-0 p-4 z-20 space-y-1"
+                              className="font-header
+                                     font-bold relative flex items-center gap-1.5 z-20"
                            >
-                              <span className="font-bold">{site.name}</span>
-                              {site.about && (
-                                 <div className="dark:text-zinc-400 text-xs">
-                                    {site.about}
-                                 </div>
+                              {site.status === "verified" && (
+                                 <Tooltip placement="top">
+                                    <TooltipTrigger>
+                                       <Icon
+                                          name="badge-check"
+                                          size={16}
+                                          className="text-teal-500"
+                                       />
+                                    </TooltipTrigger>
+                                    <TooltipContent>Verified</TooltipContent>
+                                 </Tooltip>
                               )}
+                              {site.name}
                            </div>
-                        </section>
+                           {site.about && (
+                              <div className="text-xs text-1">{site.about}</div>
+                           )}
+                        </div>
                         <Contributors site={site} />
                         {/* <div className="text-xs text-1 pb-2">Lvl 1</div>
                                  <div className="rounded-full h-3 w-full dark:bg-dark500 bg-teal-50 relative overflow-hidden">
