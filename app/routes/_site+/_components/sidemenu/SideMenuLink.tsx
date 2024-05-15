65d16777c1ff64e1932f6586479a855c5a583c8d
Nick Winters <nick@pogseal.com>
2024-05-15 15:33:56 -0400

    Update SideMenuLink.tsx

--- app/routes/_site+/_components/sidemenu/SideMenuLink.tsx
+++ app/routes/_site+/_components/sidemenu/SideMenuLink.tsx
@@ -19,12 +19,6 @@ import {
    verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { SideMenuSectionNestedLink } from "./SideMenuSectionNestedLink";
-import {
-   Dropdown,
-   DropdownButton,
-   DropdownMenu,
-   DropdownItem,
-} from "~/components/Dropdown";
 import { nanoid } from "nanoid";
 
 export function SideMenuLink({
@@ -49,6 +43,17 @@ export function SideMenuLink({
    const activeSection = link?.nestedLinks?.find(
       (x: any) => "id" in x && x.id === activeId,
    );
+
+   const {
+      attributes,
+      listeners,
+      setNodeRef,
+      transform,
+      isSorting,
+      transition,
+      isDragging,
+      setActivatorNodeRef,
+   } = useSortable({ id: link.id });
 
    function handleDragStart(event: DragStartEvent) {
       if (event.active) {
@@ -91,16 +96,7 @@ export function SideMenuLink({
       setActiveId(null);
    }
 
-   const {
-      attributes,
-      listeners,
-      setNodeRef,
-      transform,
-      isSorting,
-      transition,
-      isDragging,
-      setActivatorNodeRef,
-   } = useSortable({ id: link.id });
+   const [editMode, setEditMode] = useState(false);
 
    return (
       <>
@@ -119,6 +115,7 @@ export function SideMenuLink({
                } as React.CSSProperties /* cast because of css variable */
             }
             {...attributes}
+            onMouseLeave={() => setEditMode(false)}
          >
             {/* Insert top */}
             <div className="-top-[1px] left-0 w-full h-2 rounded-t-lg absolute opacity-0 hover:opacity-100">
@@ -179,10 +176,7 @@ export function SideMenuLink({
                </button>
             </div>
             {link.nestedLinks && link.nestedLinks.length > 0 ? (
-               <button
-                  onClick={() => setSection(!isSectionOpen)}
-                  className="flex items-center justify-between gap-2 w-full text-left "
-               >
+               editMode ? (
                   <div className="flex items-center gap-2">
                      <Avatar
                         initials={link.name.charAt(0)}
@@ -190,18 +184,58 @@ export function SideMenuLink({
                         src={link.icon}
                         options="aspect_ratio=1:1&height=120&width=120"
                      />
-                     <span className="text-[13px] font-semibold text-zinc-500 dark:text-zinc-400 truncate">
-                        {link.name}
-                     </span>
-                     <Icon
-                        name="chevron-right"
-                        className={`${
-                           isSectionOpen ? "rotate-90" : ""
-                        } transform transition duration-300 ease-in-out text-zinc-400 dark:text-zinc-500`}
-                        size={14}
+                     <input
+                        autoFocus
+                        type="text"
+                        defaultValue={link.name}
+                        className="bg-transparent w-full max-w-full focus:ring-1 bg-white ring-zinc-300
+                   dark:ring-zinc-600 dark:bg-dark450 rounded text-[13px] 
+                     font-semibold text-zinc-500 dark:text-zinc-400 focus:outline-none px-1"
                      />
                   </div>
-               </button>
+               ) : (
+                  <button
+                     onClick={() => setSection(!isSectionOpen)}
+                     className="flex items-center justify-between gap-2 w-full text-left "
+                  >
+                     <div className="flex items-center gap-2">
+                        <Avatar
+                           initials={link.name.charAt(0)}
+                           className="size-6 flex-none"
+                           src={link.icon}
+                           options="aspect_ratio=1:1&height=120&width=120"
+                        />
+
+                        <span className="text-[13px] font-semibold text-zinc-500 dark:text-zinc-400 truncate">
+                           {link.name}
+                        </span>
+                        <Icon
+                           name="chevron-right"
+                           className={`${
+                              isSectionOpen ? "rotate-90" : ""
+                           } transform transition duration-300 ease-in-out text-zinc-400 dark:text-zinc-500`}
+                           size={14}
+                        />
+                     </div>
+                  </button>
+               )
+            ) : editMode ? (
+               <div className="flex items-center gap-2">
+                  <Avatar
+                     initials={link.name.charAt(0)}
+                     className="size-6 flex-none"
+                     src={link.icon}
+                     options="aspect_ratio=1:1&height=120&width=120"
+                  />
+                  <input
+                     autoFocus
+                     type="text"
+                     defaultValue={link.name}
+                     className="bg-transparent w-full max-w-full focus:ring-1 bg-white ring-zinc-300
+                     dark:ring-zinc-600 dark:bg-dark450 rounded text-[13px] 
+                     font-semibold text-zinc-500 dark:text-zinc-400 focus:outline-none px-1"
+                  />
+               </div>
             ) : (
                <Link className="flex items-center gap-2" to={link.path}>
                   <Avatar
@@ -215,63 +249,91 @@ export function SideMenuLink({
                   </span>
                </Link>
             )}
-            <div className="flex items-center gap-0.5">
-               <Dropdown>
-                  <DropdownButton
-                     className="text-1 !size-6 group-hover/section:flex hidden !rounded-full flex-none !px-0 !justify-center"
-                     plain
-                     aria-label="Options"
+            <div className="items-center hidden flex group-hover/section:flex flex-none gap-1">
+               <button
+                  className={clsx(
+                     editMode
+                        ? "bg-green-50 dark:bg-green-950 dark:border-green-900 dark:hover:bg-green-900 dark:hover:border-green-800 border-green-300 hover:bg-green-100 group"
+                        : "dark:hover:bg-dark450 hover:bg-white border-transparent",
+                     "rounded-md size-5 border flex items-center justify-center group",
+                  )}
+                  onClick={() => setEditMode(!editMode)}
+               >
+                  {editMode ? (
+                     <Icon
+                        size={12}
+                        name="check"
+                        title="Done editing"
+                        className="text-green-500 group-hover:text-green-600 dark:group-hover:text-green-400"
+                     />
+                  ) : (
+                     <Icon
+                        size={12}
+                        name="pencil"
+                        title="Edit"
+                        className="dark:text-zinc-500 text-zinc-400 dark:group-hover:text-zinc-400 group-hover:text-zinc-500"
+                     />
+                  )}
+               </button>
+               {/* Remove Link */}
+               {editMode && (
+                  <button
+                     className="rounded-l-full border border-r-0 dark:hover:bg-red-900 dark:bg-red-950 
+                     dark:border-red-900 border-red-200 bg-red-50 hover:bg-red-100 dark:hover:border-red-800
+                     hover:border-red-200 size-5 flex items-center justify-center group -mr-3.5"
+                     onClick={() =>
+                        setMenu((existingMenuItems: any[]) => {
+                           const updatedMenu = existingMenuItems
+                              .map((menuRow: any) => {
+                                 const getLink = menuRow?.links?.find(
+                                    (x: any) => x.id === link.id,
+                                 );
+
+                                 if (getLink) {
+                                    const newMenuItems = [...menuRow?.links];
+                                    //@ts-ignore
+                                    newMenuItems.splice(index, 1);
+
+                                    return {
+                                       ...menuRow,
+                                       links: newMenuItems,
+                                    };
+                                 }
+
+                                 return { ...menuRow };
+                              })
+                              //Remove empty sections
+                              .filter(
+                                 (menuRow: any) => menuRow?.links?.length > 0,
+                              );
+                           return updatedMenu;
+                        })
+                     }
                   >
                      <Icon
-                        size={14}
-                        name="ellipsis"
-                        title="Options"
-                        className="text-zinc-400 mx-auto dark:text-zinc-500 group-hover:text-zinc-500 dark:group-hover:text-zinc-400"
+                        size={10}
+                        name="trash"
+                        title="Delete"
+                        className="text-red-400 group-hover:text-red-500 dark:group-hover:text-red-300"
                      />
-                  </DropdownButton>
-                  <DropdownMenu className="z-50" anchor="bottom">
-                     <div className="flex items-center justify-between">
-                        <DropdownItem className="!size-8 !p-0 flex-none justify-center">
-                           <Icon
-                              size={14}
-                              name="pencil"
-                              title="Edit"
-                              className="text-blue-400"
-                           />
-                        </DropdownItem>
-                        <DropdownItem className="!size-8 !p-0 flex-none justify-center">
-                           <Icon
-                              size={18}
-                              name="list-tree"
-                              title="Add"
-                              className="text-green-400"
-                           />
-                        </DropdownItem>
-                        <DropdownItem className="!size-8 !p-0 flex-none justify-center">
-                           <Icon
-                              size={14}
-                              name="trash"
-                              title="Delete"
-                              className="text-red-400"
-                           />
-                        </DropdownItem>
-                     </div>
-                  </DropdownMenu>
-               </Dropdown>
-               <div
-                  className="touch-none group-hover/section:flex cursor-grab hidden
+                  </button>
+               )}
+               {!editMode && (
+                  <div
+                     className="touch-none cursor-grab 
                   hover:bg-zinc-200 dark:hover:bg-dark500 rounded py-1 px-0.5"
-                  aria-label="Drag to reorder"
-                  ref={setActivatorNodeRef}
-                  {...listeners}
-               >
-                  <Icon
-                     name="grip-vertical"
-                     className="text-1"
-                     title="Drag to reorder"
-                     size={14}
-                  />
-               </div>
+                     aria-label="Drag to reorder"
+                     ref={setActivatorNodeRef}
+                     {...listeners}
+                  >
+                     <Icon
+                        name="grip-vertical"
+                        className="text-1"
+                        title="Drag to reorder"
+                        size={14}
+                     />
+                  </div>
+               )}
             </div>
             {/* Insert bottom */}
             <div className="-bottom-[1px] left-0 w-full h-2 rounded-t-lg absolute opacity-0 hover:opacity-100">
