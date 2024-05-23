0f961820f06610bcdfe74bc8755d748c8cd5af82
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2024-05-22 18:39:45 -0700

    [wuwa] Achievement Tracker page added
    Updates:
    - Minor fixes to Sonata Effects list page formatting
    
    New:
    - Achievement tracker using local storage added. Handles both mobile and desktop displays.

--- app/_custom/routes/_site.c+/sonata-effects.tsx
+++ app/_custom/routes/_site.c+/sonata-effects.tsx
@@ -326,14 +326,14 @@ const EntryWithDescription = ({ char }: any) => {
       >
         <div className="border-color-sub bg-2-sub shadow-1 overflow-hidden rounded-lg border shadow-sm w-full h-full">
           {/* Header with Sonata Icon and Name */}
-          <div className="bg-3-sub relative flex items-center gap-3 p-3 border-b border-color-sub">
+          <div className="bg-3-sub relative flex items-center justify-center gap-3 p-3 border-b border-color-sub">
             <div className="bg-zinc-800 dark:bg-transparent rounded-full">
               <div
                 style={{
                   "border-color": `#${sonata_color}`,
                   "background-color": `#${sonata_color}44`,
                 }}
-                className="flex h-11 w-11 items-center justify-center rounded-full border-2"
+                className="flex h-7 w-7 items-center justify-center rounded-full border-2"
               >
                 <Image
                   options="aspect_ratio=1:1&height=80&width=80"
@@ -345,8 +345,11 @@ const EntryWithDescription = ({ char }: any) => {
               </div>
             </div>
             <div className="space-y-1">
-              <div className="font-bold">{sonata_name}</div>
+              <div className="font-bold text-blue-500">{sonata_name}</div>
             </div>
+            {/* <div className="">
+              <Icon name="external-link" className="text-zinc-500" size={16} />
+            </div> */}
           </div>
 
           {/* Set Effects */}
@@ -376,64 +379,6 @@ const EntryWithDescription = ({ char }: any) => {
           </div>
         </div>
       </Link>
-    </>
-  );
-};
-
-const EntryIconOnly = ({ char }: any) => {
-  const sonata_icon = char?.icon?.url;
-  const sonata_name = char?.name;
-  const sonata_effects = char?.effects;
-  const sonata_color = char?.color;
-  return (
-    <>
-      <div className="border-color-sub bg-2-sub shadow-1 overflow-hidden rounded-lg border shadow-sm">
-        {/* Header with Sonata Icon and Name */}
-        <div className="bg-3-sub relative flex items-center gap-3 p-3 border-b border-color-sub">
-          <div className="bg-zinc-800 dark:bg-transparent rounded-full">
-            <div
-              style={{
-                "border-color": `#${sonata_color}`,
-                "background-color": `#${sonata_color}44`,
-              }}
-              className="flex h-11 w-11 items-center justify-center rounded-full border-2"
-            >
-              <Image
-                options="aspect_ratio=1:1&height=80&width=80"
-                className="object-contain"
-                url={sonata_icon}
-                alt={sonata_name}
-                loading="lazy"
-              />
-            </div>
-          </div>
-          <div className="space-y-1">
-            <div className="font-bold">{sonata_name}</div>
-          </div>
-        </div>
-
-        {/* Set Effects */}
-        <div className="border-color-sub border-t p-3 text-sm">
-          {sonata_effects.map((effect: any) => {
-            var dispdesc = effect.effect;
-            effect.params.map((par: any, i: any) => {
-              dispdesc = dispdesc?.replace("{" + i + "}", par);
-            });
-
-            return (
-              <>
-                <div className="font-bold">{effect.pieces}-Set: </div>
-                <div
-                  className=""
-                  dangerouslySetInnerHTML={{
-                    __html: dispdesc ?? "",
-                  }}
-                ></div>
-              </>
-            );
-          })}
-        </div>
-      </div>
     </>
   );
 };
