bfc6f9be2ce354350a9a104ed79c44a43895e72e
xHomu <nobisan@gmail.com>
2024-06-22 13:20:07 -0700

    Update Main.tsx

--- app/_custom/components/namecards/Main.tsx
+++ app/_custom/components/namecards/Main.tsx
@@ -4,57 +4,68 @@ import { Image } from "~/components/Image";
 import { useState } from "react";
 
 export function Main({ data: fulldata }: { data: ItemType }) {
-  const char = fulldata;
+   const char = fulldata;
 
-  const mainImage = char?.icon?.url;
-  const mainBanner = char?.image?.url;
-  const mainName = char?.name;
-  const mainDesc = char?.desc;
-  const mainStatDisplay = [
-    {
-      label: "Rarity",
-      value: char.rarity?.id,
-    },
-  ];
+   const mainImage = char?.icon?.url;
+   const mainBanner = char?.image?.url;
+   const mainName = char?.name;
+   const mainDesc = char?.desc;
+   const mainStatDisplay = [
+      {
+         label: "Rarity",
+         value: char.rarity?.id,
+      },
+   ];
 
-  return (
-    <>
-      <div className="laptop:grid laptop:grid-cols-2 laptop:gap-4">
-        <section>
-          <div className="flex items-center justify-center shadow-sm shadow-1 border border-color-sub rounded-lg dark:bg-dark350 bg-zinc-50 h-full p-3">
-            <Image
-              height={280}
-              className="object-contain"
-              url={mainImage ?? "https://static.mana.wiki/zzz/Run2_00016.png"}
-              options="height=280"
-              alt={mainName ?? "Icon"}
-            />
-          </div>
-        </section>
-        <section>
-          <div
-            className="border border-color-sub divide-y divide-color-sub shadow-sm shadow-1 rounded-lg 
+   return (
+      <>
+         <div className="laptop:grid laptop:grid-cols-2 laptop:gap-4">
+            <section>
+               <div className="flex items-center justify-center shadow-sm shadow-1 border border-color-sub rounded-lg dark:bg-dark350 bg-zinc-50 h-full p-3">
+                  <Image
+                     height={280}
+                     width={280}
+                     className="object-contain"
+                     url={
+                        mainImage ??
+                        "https://static.mana.wiki/zzz/Run2_00016.png"
+                     }
+                     alt={mainName ?? "Icon"}
+                  />
+               </div>
+            </section>
+            <section>
+               <div
+                  className="border border-color-sub divide-y divide-color-sub shadow-sm shadow-1 rounded-lg 
           [&>*:nth-of-type(odd)]:bg-zinc-50 dark:[&>*:nth-of-type(odd)]:bg-dark350 overflow-hidden"
-          >
-            <div className="flex items-center justify-center shadow-sm shadow-1 dark:bg-dark350 bg-zinc-50 h-full p-3">
-              <Image
-                height={280}
-                className="object-contain"
-                url={
-                  mainBanner ?? "https://static.mana.wiki/zzz/Run2_00016.png"
-                }
-                options="height=280"
-                alt={mainName ?? "Icon"}
-              />
-            </div>
-            {mainStatDisplay?.map((row) => (
-              <div className="px-3 py-2 justify-between flex items-center gap-2">
-                <div className="flex items-center gap-2">
-                  <span className="font-semibold text-sm">{row.label}</span>
-                </div>
-                <div className="text-sm font-semibold">
-                  <span className="inline-block align-middle">{row.value}</span>
-                  {/* {row.icon ? (
+               >
+                  <div className="flex items-center justify-center shadow-sm shadow-1 dark:bg-dark350 bg-zinc-50 h-full p-3">
+                     <Image
+                        height={280}
+                        width={280}
+                        className="object-contain"
+                        url={
+                           mainBanner ??
+                           "https://static.mana.wiki/zzz/Run2_00016.png"
+                        }
+                        alt={mainName ?? "Icon"}
+                     />
+                  </div>
+                  {mainStatDisplay?.map((row) => (
+                     <div
+                        className="px-3 py-2 justify-between flex items-center gap-2"
+                        key={row.label}
+                     >
+                        <div className="flex items-center gap-2">
+                           <span className="font-semibold text-sm">
+                              {row.label}
+                           </span>
+                        </div>
+                        <div className="text-sm font-semibold">
+                           <span className="inline-block align-middle">
+                              {row.value}
+                           </span>
+                           {/* {row.icon ? (
                     <>
                       <div className="items-center inline-block align-middle rounded-md justify-center dark:bg-dark350 bg-zinc-600 h-full ml-2">
                         <Image
@@ -67,19 +78,19 @@ export function Main({ data: fulldata }: { data: ItemType }) {
                       </div>
                     </>
                   ) : null} */}
-                </div>
-              </div>
-            ))}
+                        </div>
+                     </div>
+                  ))}
 
-            {/* Description */}
-            {mainDesc == "" ? null : (
-              <div className="px-3 py-2 justify-between flex items-center gap-2 text-sm">
-                {mainDesc}
-              </div>
-            )}
-          </div>
-        </section>
-      </div>
-    </>
-  );
+                  {/* Description */}
+                  {mainDesc == "" ? null : (
+                     <div className="px-3 py-2 justify-between flex items-center gap-2 text-sm">
+                        {mainDesc}
+                     </div>
+                  )}
+               </div>
+            </section>
+         </div>
+      </>
+   );
 }
