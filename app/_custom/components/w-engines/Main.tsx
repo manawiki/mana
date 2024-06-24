abbffc4a4aedd2545a6a3834039b00dcbc24c9e0
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2024-06-24 14:41:11 -0700

    [ZZZ] W Engine Stat Slider and calculation added.
    - Loading of weapon star and level curve data added to entry template
    - Added weapon and stat / ascension status sliders + main and secondary stat calculations

--- app/_custom/components/w-engines/Main.tsx
+++ app/_custom/components/w-engines/Main.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-
-import { Disclosure } from "@headlessui/react";
+import { Input } from "~/components/Input";
 
 import type { WEngine as WEngineType } from "payload/generated-custom-types";
 import { Image } from "~/components/Image";
 
-export function Main({ data: char }: { data: WEngineType }) {
-   const [level, setLevel] = useState(0);
+export function Main({ data }: { data: WEngineType }) {
+   const [level, setLevel] = useState(60);
+   const [levelAscensionCheck, setLevelAscensionCheck] = useState(true);
+
+   const char = data.entry?.data?.WEngine;
 
    const mainImage = char?.icon_full?.url;
    const mainName = char?.name;
@@ -17,15 +19,46 @@ export function Main({ data: char }: { data: WEngineType }) {
          icon: char.rarity?.icon_item?.url,
       },
    ];
+
+   const asc_index =
+      Math.floor(level / 10) +
+      ((!levelAscensionCheck && level / 10 == Math.floor(level / 10)) ||
+      level == 60
+         ? -1
+         : 0);
+
+   const dispasc = asc_index + 1;
+
+   const wlevel = data.wLevelData?.find(
+      (j: any) => j.name == "WeaponLevelTemplateTb",
+   )?.json;
+   const wstar = data.wLevelData?.find(
+      (j: any) => j.name == "WeaponStarTemplateTb",
+   )?.json;
+
+   const level_growth = wlevel?.find(
+      (w: any) => w.rarity == char?.rarity?.id && w.level == level,
+   )?.growth;
+   const star_growth = wstar?.filter(
+      (w: any) => w.rarity == char?.rarity?.id,
+   )?.[asc_index]?.growth;
+   const secondary_growth = wstar?.filter(
+      (w: any) => w.rarity == char?.rarity?.id,
+   )?.[asc_index]?.second_stat_mod;
+
    const basicStatDisplay = [
       {
-         label: char?.stat_primary?.name,
-         value: char?.stat_primary?.value,
+         label: char?.stat_primary?.name?.replace("Base ", ""),
+         value: calcMainStat(
+            char?.stat_primary?.value,
+            level_growth,
+            star_growth,
+         ),
          //icon: "https://static.mana.wiki/zzz/IconHpMax.png",
       },
       {
          label: char?.stat_secondary?.name,
-         value: char?.stat_secondary?.value,
+         value: calcSecondStat(char?.stat_secondary?.value, secondary_growth),
          //icon: "https://static.mana.wiki/zzz/IconAttack.png",
       },
    ];
@@ -80,9 +113,103 @@ export function Main({ data: char }: { data: WEngineType }) {
                      </div>
                   ))}
 
-                  {basicStatDisplay?.map((stat) => <StatBlock attr={stat} />)}
+                  {basicStatDisplay?.map((stat) => (
+                     <StatBlock attr={stat} />
+                  ))}
                </div>
             </section>
+         </div>
+
+         {/* ======================== */}
+         {/* Stats Slider and additional top info block */}
+         {/* ======================== */}
+         {/* 2a) Header for Adjusting Level and Slider */}
+         {/* ======================== */}
+         <div className="bg-2-sub shadow-1 border-color-sub my-3 rounded-lg border px-6 py-3 font-bold shadow-sm">
+            <div className="flex w-full items-center justify-between text-center">
+               <div className="flex items-center gap-3">
+                  {/* Level Label */}
+                  <div className="inline-flex justify-between align-middle">
+                     Lvl
+                  </div>
+                  {/* Level Input Box */}
+                  <Input
+                     className="!w-16 flex-none"
+                     type="number"
+                     aria-label="Level"
+                     value={level}
+                     onChange={(event) => {
+                        const numonly = /^[0-9\b]+$/;
+                        const maxval = 60;
+
+                        // Only set the level slider value if the entered value is not blank or a Number. Parseint as well so leading 0s are removed.
+                        if (numonly.test(event.target.value)) {
+                           let input = parseInt(event.target.value);
+                           if (input > maxval) {
+                              input = maxval;
+                           } else if (input < 1) {
+                              input = 1;
+                           }
+                           setLevel(input);
+                        }
+                     }}
+                  />
+                  {/* ◇ ◆ ☆ ★ Symbols */}
+                  <div className="flex text-md font-bold px-1 items-center self-center rounded-full bg-zinc-500 h-6">
+                     {[2, 3, 4, 5, 6].map((stg: any) => (
+                        <div
+                           className={`inline-block text-sm align-middle drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)] ${
+                              dispasc >= stg ? "text-white" : "text-black"
+                           }`}
+                        >
+                           ★
+                        </div>
+                        // <Image
+                        //   height={15}
+                        //   className={`object-contain inline-block align-middle drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)] ${
+                        //     dispasc >= stg ? "invert-[0.10]" : "invert-[0.90]"
+                        //   }`}
+                        //   url={"https://static.mana.wiki/endfield/icon_breakmark_01.png"}
+                        //   options="height=15"
+                        //   alt={">"}
+                        // />
+                     ))}
+                  </div>
+                  {/* Asc Label */}
+                  <div className="inline-flex justify-between align-middle text-sm">
+                     Asc
+                  </div>
+                  {/* Ascension Checkbox */}
+                  <input
+                     className="mr-2 inline-flex h-6 w-6 flex-shrink-0 items-center 
+                     justify-between rounded-sm align-middle text-zinc-500"
+                     type="checkbox"
+                     aria-label="Ascension"
+                     disabled={
+                        // [20, 40, 60, 70, 80, 90].indexOf(levelSliderValue) < -1
+                        ["10", "20", "30", "40", "50"].indexOf(
+                           level.toString(),
+                        ) > -1
+                           ? false
+                           : true
+                     }
+                     checked={levelAscensionCheck}
+                     onChange={(event) =>
+                        setLevelAscensionCheck(event.target.checked)
+                     }
+                  ></input>
+               </div>
+               {/* Slider */}
+               <input
+                  aria-label="Level Slider"
+                  className="flex-grow"
+                  type="range"
+                  min="1"
+                  max="60"
+                  value={level}
+                  onChange={(event) => setLevel(parseInt(event.target.value))}
+               ></input>
+            </div>
          </div>
       </>
    );
@@ -124,3 +251,11 @@ const StatBlock = ({ attr }: any) => {
       </>
    );
 };
+
+function calcMainStat(base: any, level_growth: any, star_growth: any) {
+   return Math.floor(base * (level_growth / 10000 + 1 + star_growth / 10000));
+}
+
+function calcSecondStat(base: any, secondary_growth: any) {
+   return Math.floor(base * (1 + secondary_growth / 10000));
+}
