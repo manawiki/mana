67da6279d135f7184df90420f21005c850b5a5e1
Dimbreath <dimbreath@gmail.com>
2024-07-01 15:54:44 -0300

    Comment it out

--- app/_custom/routes/_site+/achievement-tracker.tsx
+++ app/_custom/routes/_site+/achievement-tracker.tsx
@@ -1,45 +1,44 @@
 
-const ACHIEVEMENTS = gql`
-query {
-  AchievementGroups(limit:0) {
-    docs {
-      id
-    	name
-    }
-  }
-  AchievementSubgroups(limit: 0) {
-    docs {
-      id
-    	name
-      group {
-        id
-      }
-    }
-  }
-  Achievements(limit:0) {
-    docs {
-      name
-      desc
-      rarity {
-        id
-        icon {
-          url
-        }
-      }
-      subgroup {
-        id
-      }
-      rewards {
-        item {
-          id
-          name
-          icon {
-            url
-          }
-        }
-        qty
-      }
-    }
-  }
-}
-`;
+//const ACHIEVEMENTS = gql`
+//query {
+//  AchievementGroups(limit:0) {
+//    docs {
+//      id
+//    	name
+//    }
+//  }
+//  AchievementSubgroups(limit: 0) {
+//    docs {
+//      id
+//    	name
+//      group {
+//        id
+//      }
+//    }
+//  }
+//  Achievements(limit:0) {
+//    docs {
+//      name
+//      desc
+//      rarity {
+//        id
+//        icon {
+//          url
+//        }
+//      }
+//      subgroup {
+//        id
+//      }
+//      rewards {
+//        item {
+//          id
+//          name
+//          icon {
+//            url
+//          }
+//        }
+//        qty
+//      }
+//    }
+//  }
+//}`;
