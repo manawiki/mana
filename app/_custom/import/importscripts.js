80f24b29bba92494bdf98a933bc30228c7d848f4
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2024-07-26 20:30:14 -0700

    [grandorder] Material Templating Completed
    All subsections and math / totals completed:
    - Main
    - Drop Locations
    - Ascension and Skill Material Servant Requirements (all 6 total breakdowns)
    
    Minor Importscripts updates.

--- app/_custom/import/importscripts.js
+++ app/_custom/import/importscripts.js
@@ -1,13 +1,60 @@
 #"import_collection_data": "cross-env PAYLOAD_CONFIG_PATH=./app/db/payload.custom.config.ts ts-node -T app/_custom/import/import_collection_data.ts"
+#"import_servant_stats": "cross-env PAYLOAD_CONFIG_PATH=./app/db/payload.custom.config.ts ts-node -T app/_custom/import/import_301a_ServantStats.ts"
+
+
 
 # Items
 pnpm import_collection_data collection:materials,filename:Item.json,idname:drupal_nid,sync:false,overwrite:false
+
+# _EnemyTraits
+pnpm import_collection_data collection:_enemy-traits,filename:_EnemyTrait.json,idname:drupal_tid,sync:false,overwrite:false
+# _Trait
+pnpm import_collection_data collection:_traits,filename:_Trait.json,idname:drupal_tid,sync:false,overwrite:false
+# _StatusEffect
+pnpm import_collection_data collection:_status-effects,filename:_StatusEffect.json,idname:drupal_tid,sync:false,overwrite:false
+# _BuffCategory
+pnpm import_collection_data collection:_buff-categories,filename:_BuffCategory.json,idname:drupal_tid,sync:false,overwrite:false
+# _SkillClassificationSpecific
+pnpm import_collection_data collection:_skill-classification-specifics,filename:_SkillClassificationSpecific.json,idname:drupal_tid,sync:false,overwrite:false
+# _SkillImage
+pnpm import_collection_data collection:_skill-images,filename:_SkillImage.json,idname:drupal_tid,sync:false,overwrite:false
+# _Tag
+pnpm import_collection_data collection:_tags,filename:_Tag.json,idname:drupal_tid,sync:false,overwrite:false
+
+# _CV
+pnpm import_collection_data collection:_cvs,filename:_CV.json,idname:data_key,sync:false,overwrite:false
+# _Illustrator
+pnpm import_collection_data collection:_illustrators,filename:_Illustrator.json,idname:data_key,sync:false,overwrite:false
+
+# ServantSkill
+pnpm import_collection_data collection:servant-skills,filename:ServantSkill.json,idname:drupal_nid,sync:false,overwrite:false
+
+# _ClassSkillType
+pnpm import_collection_data collection:_class-skill-types,filename:_ClassSkillType.json,idname:drupal_tid,sync:false,overwrite:false
+# ClassSkill
+pnpm import_collection_data collection:class-skills,filename:ClassSkill.json,idname:drupal_nid,sync:false,overwrite:false
+
+# NoblePhantasm
+pnpm import_collection_data collection:noble-phantasms,filename:NoblePhantasm.json,idname:drupal_nid,sync:false,overwrite:false
+
+# _Chapter
+pnpm import_collection_data collection:_chapters,filename:_Chapter.json,idname:drupal_tid,sync:false,overwrite:false
+
+# Servant
+pnpm import_collection_data collection:servants,filename:Servant.json,idname:drupal_nid,sync:false,overwrite:false
+# ServantStat
+pnpm import_collection_data collection:servants,filename:ServantStat.json,idname:drupal_nid,sync:false,overwrite:false
+
+# CE
+pnpm import_collection_data collection:craft-essences,filename:CE.json,idname:drupal_nid,sync:true,overwrite:false
 
 # Importing Costumes (301b) and later
 pnpm import_collection_data collection:costumes,filename:Costume.json,idname:drupal_nid,sync:false,overwrite:false
 
 # Command Codes
 pnpm import_collection_data collection:command-codes,filename:CommandCode.json,idname:drupal_nid,sync:true,overwrite:false
+
+
 
 # _Class Rarities
 pnpm import_collection_data collection:_class-rarities,filename:_ClassRarity.json,idname:drupal_tid,sync:false,overwrite:false
@@ -34,6 +81,8 @@ pnpm import_collection_data collection:mystic-codes,filename:MysticCode.json,idn
 
 # Quests
 pnpm import_collection_data collection:quests,filename:Quest.json,idname:drupal_nid,sync:false,overwrite:false
+# _BreakBars
+pnpm import_collection_data collection:_break-bars,filename:_BreakBar.json,idname:drupal_tid,sync:false,overwrite:false
 
 # Banners
 pnpm import_collection_data collection:summon-events,filename:SummonEvent.json,idname:drupal_nid,sync:false,overwrite:false
---
+++ app/_custom/routes/_site.c+/materials_.$entryId.tsx
@@ -0,0 +1,214 @@
+// Core Imports
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { gql } from "graphql-request";
+import { Entry } from "~/routes/_site+/c_+/$collectionId_.$entryId/components/Entry";
+import { entryMeta } from "~/routes/_site+/c_+/$collectionId_.$entryId/utils/entryMeta";
+import { fetchEntry } from "~/routes/_site+/c_+/$collectionId_.$entryId/utils/fetchEntry.server";
+
+export { entryMeta as meta };
+
+// Custom Site / Collection Config Imports
+import type { Material as MaterialType } from "~/db/payload-custom-types";
+
+// Custom Component Imports
+import { Main } from "~/_custom/components/materials/Main";
+import { DropLocation } from "~/_custom/components/materials/DropLocation";
+import { ServantAscSkill } from "~/_custom/components/materials/ServantAscSkill";
+
+// Loader definition - loads Entry data!
+export async function loader({
+   context: { payload, user },
+   params,
+   request,
+}: LoaderFunctionArgs) {
+   const fetchCharacterData = fetchEntry({
+      payload,
+      params,
+      request,
+      user,
+      gql: {
+         query: QUERY,
+      },
+   });
+
+   const fetchAscensionData = fetchEntry({
+      payload,
+      params,
+      request,
+      user,
+      gql: {
+         query: SERVANT_ASCENSION_QUERY,
+      },
+   });
+   const featchSkillData = fetchEntry({
+      payload,
+      params,
+      request,
+      user,
+      gql: {
+         query: SERVANT_SKILL_QUERY,
+      },
+   });
+   const fetchAppendData = fetchEntry({
+      payload,
+      params,
+      request,
+      user,
+      gql: {
+         query: SERVANT_APPEND_QUERY,
+      },
+   });
+
+   const [{ entry }, ascension, skill, append] = await Promise.all([
+      fetchCharacterData,
+      fetchAscensionData,
+      featchSkillData,
+      fetchAppendData,
+   ]);
+
+   return json({
+      entry,
+      material: entry?.data?.Servant,
+      ascension: ascension?.entry?.data?.Servants?.docs,
+      skill: skill?.entry?.data?.Servants?.docs,
+      append: append?.entry?.data?.Servants?.docs,
+   });
+}
+
+const SECTIONS = {
+   main: Main,
+   "drop-locations": DropLocation,
+   "servant-requirements": ServantAscSkill,
+};
+
+export default function EntryPage() {
+   const loaderdata = useLoaderData<typeof loader>();
+   console.log(loaderdata);
+
+   return <Entry customComponents={SECTIONS} customData={loaderdata} />;
+}
+
+const QUERY = gql`
+   query ($entryId: String!) {
+      Material(id: $entryId) {
+         id
+         name
+         icon {
+            url
+         }
+         description
+         _item_Type {
+            name
+         }
+         _rarity {
+            name
+         }
+         best_drop_locations {
+            ap_per_drop
+            quest_dropped_from {
+               id
+               name
+               main_quest_chapter {
+                  name
+               }
+               main_quest {
+                  id
+                  name
+               }
+            }
+         }
+      }
+   }
+`;
+
+const SERVANT_ASCENSION_QUERY = gql`
+   query ($entryId: JSON!) {
+      Servants(
+         where: {
+            ascension_materials__materials__material: { equals: $entryId }
+         }
+         sort: "library_id"
+         limit: 1000
+      ) {
+         docs {
+            id
+            library_id
+            name
+            icon {
+               url
+            }
+            ascension_materials {
+               materials {
+                  material {
+                     id
+                     name
+                  }
+                  qty
+               }
+            }
+         }
+      }
+   }
+`;
+
+const SERVANT_SKILL_QUERY = gql`
+   query ($entryId: JSON!) {
+      Servants(
+         where: {
+            skill_enhancements__materials__material: { equals: $entryId }
+         }
+         sort: "library_id"
+         limit: 1000
+      ) {
+         docs {
+            id
+            library_id
+            name
+            icon {
+               url
+            }
+            skill_enhancements {
+               materials {
+                  material {
+                     id
+                     name
+                  }
+                  qty
+               }
+            }
+         }
+      }
+   }
+`;
+
+const SERVANT_APPEND_QUERY = gql`
+   query ($entryId: JSON!) {
+      Servants(
+         where: {
+            append_skill_enhancements__materials__material: { equals: $entryId }
+         }
+         sort: "library_id"
+         limit: 1000
+      ) {
+         docs {
+            id
+            library_id
+            name
+            icon {
+               url
+            }
+            append_skill_enhancements {
+               materials {
+                  material {
+                     id
+                     name
+                  }
+                  qty
+               }
+            }
+         }
+      }
+   }
+`;
