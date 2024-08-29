08ff99c84f46c0a5b14b6c50cb4d59e0fd965964
xHomu <nobisan@gmail.com>
2024-08-29 16:03:36 -0700

    fix gtag pageview

--- app/routes/_site+/_components/GAScripts.tsx
+++ app/routes/_site+/_components/GAScripts.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+
+import { useLocation } from "@remix-run/react";
+
 import { useIsBot } from "~/utils/isBotProvider";
 
 declare global {
    interface Window {
+      gtag: (
+         option: string,
+         gaTrackingId: string,
+         options: Record<string, unknown>,
+      ) => void;
       dataLayer?: Object[];
    }
 }
@@ -12,6 +21,13 @@ export function GAScripts({
    gaTrackingId: string | undefined | null;
 }) {
    let isBot = useIsBot();
+   const location = useLocation();
+
+   useEffect(() => {
+      if (process.env.NODE_ENV === "production" && gaTrackingId) {
+         pageview(location.pathname, gaTrackingId);
+      }
+   }, [location, gaTrackingId]);
 
    return (
       process.env.NODE_ENV === "production" &&
@@ -39,3 +55,44 @@ export function GAScripts({
       )
    );
 }
+
+/**
+ * @example
+ * https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+ */
+export const pageview = (url: string, trackingId: string) => {
+   if (!window.gtag) {
+      console.warn(
+         "window.gtag is not defined. This could mean your google analytics script has not loaded on the page yet.",
+      );
+      return;
+   }
+   window.gtag("config", trackingId, {
+      page_path: url,
+   });
+   console.log(`pageview: ${url}`);
+};
+
+/**
+ * @example
+ * https://developers.google.com/analytics/devguides/collection/gtagjs/events
+ */
+export const event = ({
+   action,
+   category,
+   label,
+   value,
+}: Record<string, string>) => {
+   if (!window.gtag) {
+      console.warn(
+         "window.gtag is not defined. This could mean your google analytics script has not loaded on the page yet.",
+      );
+      return;
+   }
+   window.gtag("event", action!, {
+      event_category: category,
+      event_label: label,
+      value: value,
+   });
+   console.log(`event: ${action}`);
+};
