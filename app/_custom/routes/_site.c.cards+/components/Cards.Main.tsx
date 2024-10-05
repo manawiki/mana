9611b79e6dd4e5dc8b5bfec49e9e3325af0fdc0a
Nick Winters <nick@pogseal.com>
2024-10-05 01:28:58 -0700

    card templating

--- app/_custom/routes/_site.c.cards+/components/Cards.Main.tsx
+++ app/_custom/routes/_site.c.cards+/components/Cards.Main.tsx
@@ -1,22 +1,18 @@
 import { Card } from "~/db/payload-custom-types";
 
+import { Image } from "~/components/Image";
+
 export function CardsMain({ data }: { data: Card }) {
    const card = data;
 
    return (
-      <div className="laptop:grid laptop:grid-cols-2 pb-4 laptop:gap-4">
-         <section>
-            <div className="bg-2-sub shadow-sm shadow-1 border rounded-lg border-color-sub mb-3 relative z-20">
-               {/* <Image
-                  height={240}
-                  width={240}
-                  url={selectedImage?.imageUrl}
-                  options="height=400"
-                  alt={selectedImage?.name}
-               /> */}
-            </div>
-         </section>
-         <section className="mb-8">
+      <div className="tablet:flex pb-4 tablet:gap-4">
+         <Image
+            className="rounded-lg overflow-hidden mx-auto max-tablet:mb-4"
+            url={card.image?.url}
+            alt={card.name ?? "Card Image"}
+         />
+         <section className="mb-8 flex-grow">
             <div
                className="border border-color-sub divide-y divide-color-sub shadow-sm shadow-1 rounded-lg 
                mb-3 [&>*:nth-of-type(odd)]:bg-zinc-50 dark:[&>*:nth-of-type(odd)]:bg-dark350 overflow-hidden"
