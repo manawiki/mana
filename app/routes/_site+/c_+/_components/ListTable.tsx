b7067081b67c981fd47d3738f257ea4278ae54b5
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2024-12-18 16:40:11 -0800

    [ListTable] Set to update Column Viewability of dynamically assigned

--- app/routes/_site+/c_+/_components/ListTable.tsx
+++ app/routes/_site+/c_+/_components/ListTable.tsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo, useState } from "react";
+import { useContext, useMemo, useState, useEffect } from "react";
 
 import {
    useReactTable,
@@ -79,6 +79,7 @@ export function ListTable({
    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
       columnViewability ?? {},
    );
+
    const [pagination, setPagination] = useState<PaginationState>({
       pageIndex: 0,
       pageSize: pageSize,
@@ -113,6 +114,10 @@ export function ListTable({
       onColumnVisibilityChange: setColumnVisibility,
       globalFilterFn: globalFilterFn ?? fuzzyFilter,
    });
+
+   useEffect(() => {
+      setColumnVisibility(columnViewability ?? {});
+   }, [columnViewability]);
 
    return (
       <>
