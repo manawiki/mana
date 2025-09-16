f9daae223a2c931a6081170cd2c80f2871646158
NorseFTX <55260779+NorseFTX@users.noreply.github.com>
2025-09-15 19:17:32 -0700

    [typesense] TypesenseActions - collection initialization, revert MB alloc

--- scripts/typesenseActions.ts
+++ scripts/typesenseActions.ts
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 const initTypesenseSchema = async () => {
    let client = new Typesense.Client({
-      apiKey: process.env.TYPESENSE_PRIVATE_KEY ?? "", // Be sure to use an API key that only allows search operations
+      apiKey: process.env.TYPESENSE_PRIVATE_KEY ?? "placeholder", // Be sure to use an API key that only allows search operations
       nodes: [
          {
             host: settings?.typesenseHost ?? "search.mana.wiki",
@@ -26,14 +26,137 @@ const initTypesenseSchema = async () => {
    //    actions: ["documents:search"],
    //    collections: ["*"],
    // });
+   // console.log("searchOnlyKey:");
    // console.log(searchOnlykey);
 
-   const adminOnlykey = await client.keys().create({
-      description: "Admin key.",
-      actions: ["*"],
-      collections: ["*"],
-   });
-   console.log(adminOnlykey);
+   // const adminOnlykey = await client.keys().create({
+   //    description: "Admin key.",
+   //    actions: ["*"],
+   //    collections: ["*"],
+   // });
+   // console.log("adminOnlykey:");
+   // console.log(adminOnlykey);
+
+   // ====
+   // Define Schema
+   // ====
+
+   // let schema = {
+   //    name: "entries",
+   //    fields: [
+   //       {
+   //          name: "name",
+   //          type: "string",
+   //       },
+   //       {
+   //          name: "category",
+   //          type: "string",
+   //          facet: true,
+   //       },
+   //       {
+   //          name: "collection",
+   //          type: "string",
+   //       },
+   //       {
+   //          name: "site",
+   //          type: "string",
+   //       },
+   //    ],
+   // };
+
+   // let customPages_schema = {
+   //    name: "customPages",
+   //    fields: [
+   //       {
+   //          name: "name",
+   //          type: "string",
+   //       },
+   //       {
+   //          name: "category",
+   //          type: "string",
+   //       },
+   //       {
+   //          name: "site",
+   //          type: "string",
+   //       },
+   //    ],
+   // };
+
+   // let collections_schema = {
+   //    name: "collections",
+   //    fields: [
+   //       {
+   //          name: "name",
+   //          type: "string",
+   //       },
+   //       {
+   //          name: "site",
+   //          type: "string",
+   //       },
+   //    ],
+   // };
+
+   // let posts_schema = {
+   //    name: "posts",
+   //    fields: [
+   //       {
+   //          name: "name",
+   //          type: "string",
+   //       },
+   //       {
+   //          name: "category",
+   //          type: "string",
+   //          facet: true,
+   //       },
+   //       {
+   //          name: "site",
+   //          type: "string",
+   //       },
+   //    ],
+   // };
+
+   // await client.collections().create(schema);
+   // await client.collections().create(customPages_schema);
+   // await client.collections().create(collections_schema);
+   // await client.collections().create(posts_schema);
+
+   // ====
+   // Update Schema
+   // ====
+   // let update_schema = {
+   //    fields: [
+   //       {
+   //          name: "collection",
+   //          type: "string",
+   //       },
+   //    ],
+   // };
+   // await client.collections("entries").update(update_schema);
+
+   // ====
+   // List Collections
+   // ====
+   // const collectionList = await client.collections().retrieve();
+   // console.log(collectionList);
+
+   // await client
+   //    .collections("entries")
+   //    .documents()
+   //    .upsert({
+   //       id: "servants-J959gyNV53-31",
+   //       name: "Mash Kyrielight",
+   //       relativeURL: "/c/servants/mash-kyrielight",
+   //       absoluteURL:
+   //          "https://grandorder.gamepress.gg/c/servants/mash-kyrielight",
+   //       site: "J959gyNV53",
+   //       collection: "servants",
+   //       category: "Entry",
+   //       ...{
+   //          icon: {
+   //             url: "https://static.mana.wiki/grandorder/001_Mash_Kyrielight_Icon.png",
+   //          },
+   //       },
+   //    });
 
    try {
       // console.log(await client.keys().retrieve());
